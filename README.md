ded7ea7b3e39751bd64cdad6ca272fc2e162174f
Topi Penttinen <topi.penttinen@dimenteq.fi>
2018-05-08 13:48:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kiinteistö- ja sopimusrekisteri
---
+++ src/main/java/fi/dimenteq/ksr/KsrApplication.java
@@ -0,0 +1,12 @@
+package fi.dimenteq.ksr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsrApplication.class, args);
+	}
+}
