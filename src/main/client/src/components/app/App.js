0aace78a5f84a42e27575d2de2debe6c668c6cdf
Jere Pallaskorpi <jere.pallaskorpi@dimenteq.fi>
2018-05-09 11:37:41 +0300

    Add client structure

---
+++ src/main/client/src/components/app/App.js
@@ -0,0 +1,33 @@
+// @flow
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { injectGlobal } from 'styled-components';
+import * as styles from '../ui/defaultStyles';
+import Home from './home/Home';
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route path="/" component={Home} />
+        </Switch>
+    </Router>
+);
+
+injectGlobal([`
+  body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+    font-size: 16px;
+    background: ${styles.colorBackgroundLightGray};
+    color: ${styles.colorFontDark};
+    
+    input, select, textarea, button {
+        font-family: inherit;
+    }
+    
+    *::-moz-selection { background: ${styles.colorMain}; }
+    *::selection { background: ${styles.colorMain}; }
+  }
+`]);
+
+export default App;
