0ac3592a21142d883354b15485d63e25699e4868
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-05-16 13:36:56 +0300

    Add log4j2 for logging

--- src/main/java/fi/sitowise/ksr/KsrApplication.java
+++ src/main/java/fi/sitowise/ksr/KsrApplication.java
@@ -1,12 +1,19 @@
 package fi.sitowise.ksr;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KsrApplication {
 
+	private static Logger logger = LogManager.getLogger(KsrApplication.class);
+
 	public static void main(String[] args) {
+		
+		logger.info("Entering application.");
 		SpringApplication.run(KsrApplication.class, args);
+
 	}
 }
