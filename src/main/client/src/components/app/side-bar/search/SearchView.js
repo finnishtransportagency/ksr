9b89e86b2b055b0fa555df81be783e1f27467b2d
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-05-22 10:36:11 +0300

    Add example search view

---
+++ src/main/client/src/components/app/side-bar/search/SearchView.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { H1, Button } from '../../../ui/elements';
+
+const SearchView = () => (
+    <Fragment>
+        <H1>Haku</H1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <input type="text" />
+        <Button>Hae</Button>
+    </Fragment>
+);
+
+export default SearchView;
