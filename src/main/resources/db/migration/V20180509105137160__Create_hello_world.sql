eb7fefce5dc71080bb21416caf09ad254c1b57c2
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-05-23 16:32:29 +0300

    Use Oracle database instead of PosgreSQL

--- src/main/resources/db/migration/V20180509105137160__Create_hello_world.sql
+++ src/main/resources/db/migration/V20180509105137160__Create_hello_world.sql
@@ -1,4 +1,20 @@
 create table hello_world (
-    id serial primary key,
-    message text not null
+    id number(10) not null,
+    message varchar2(100) not null
 );
+
+alter table hello_world
+    add (
+        constraint hello_world_pk primary key (id)
+    );
+
+create sequence hello_world_seq;
+
+create or replace trigger hello_world_on_insert
+    before insert on hello_world
+    for each row
+begin
+    select hello_world_seq.nextval
+    into :new.id
+    from dual;
+end;
