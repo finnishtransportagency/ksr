91a8191b7fdb88b428141de42d6ebf8a6a56c718
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-05-25 12:31:06 +0300

    KSR-128 Create layer_group table

--- src/main/resources/db/migration/R__Table_comments.sql
+++ src/main/resources/db/migration/R__Table_comments.sql
@@ -1,1 +1,6 @@
 -- Comments for tables and columns
+
+comment on table layer_group is 'Layer groups.';
+comment on column layer_group.id is 'Generated layer group identifier. Primary key.';
+comment on column layer_group.name is 'Name of the layer group.';
+comment on column layer_group.group_order is 'Order of the layer groups.';
