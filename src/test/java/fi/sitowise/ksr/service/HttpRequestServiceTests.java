fa6bfa2e87ab3f9a49c4bdc90ca9bc68c7436157
Topi Penttinen <topi.penttinen@dimenteq.fi>
2018-05-28 15:22:26 +0300

    KSR-127 Add unit tests for HttpRequestService

--- src/test/java/fi/sitowise/ksr/service/HttpRequestServiceTests.java
+++ src/test/java/fi/sitowise/ksr/service/HttpRequestServiceTests.java
@@ -1,4 +1,139 @@
 package fi.sitowise.ksr.service;
 
+import fi.sitowise.ksr.domain.MapLayer;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = SpringRunner.class)
+@ContextConfiguration(classes = HttpRequestService.class)
 public class HttpRequestServiceTests {
+
+    @Autowired
+    HttpRequestService httpRequestService;
+
+    @MockBean
+    CloseableHttpClient closeableHttpClient;
+
+    @MockBean
+    RequestConfig requestConfig;
+
+
+    @Test
+    public void testIsGetCapabilitiesRequest() {
+        Assert.assertEquals(true, httpRequestService.isGetCapabilitiesRequest("http://test.example.com/wms?service=wms&request=GetCapabilities"));
+        Assert.assertEquals(true, httpRequestService.isGetCapabilitiesRequest("http://test.example.com/wms?service=wms&request=getcapabilities"));
+        Assert.assertEquals(true, httpRequestService.isGetCapabilitiesRequest("http://test.example.com/wmts/1.0.0/WMTSCapabilities.xml"));
+        Assert.assertEquals(false, httpRequestService.isGetCapabilitiesRequest("http://test.example.com/wms?service=wms&request=GetMap"));
+    }
+
+    @Test
+    public void testGetRequestBase() throws URISyntaxException {
+        HttpRequestBase getBase = httpRequestService.getRequestBase("GET", "http://test.example.com/wms?service=wms&request=GetCapabilities", null);
+        Assert.assertEquals("GET", getBase.getMethod());
+        Assert.assertEquals(new java.net.URI("http://test.example.com/wms?service=wms&request=GetCapabilities"), getBase.getURI());
+    }
+
+    @Test
+    public void testSetResponseHeaders() {
+        MockHttpServletResponse res = new MockHttpServletResponse();
+        CloseableHttpResponse cRes = Mockito.mock(CloseableHttpResponse.class);
+        Mockito.when(cRes.getFirstHeader("Content-Type")).thenReturn(new BasicHeader("Content-Type", "image/png"));
+        Mockito.when(cRes.getFirstHeader("Expires")).thenReturn(new BasicHeader("Expires", "Wed, 21 Oct 2045 07:28:00 GMT"));
+        Mockito.when(cRes.getFirstHeader("Content-Length")).thenReturn(null);
+        Mockito.when(cRes.getFirstHeader("Cache-control")).thenReturn(null);
+        Mockito.when(cRes.getFirstHeader("Last-Modified")).thenReturn(null);
+
+        httpRequestService.setResponseHeaders(res, cRes);
+
+        Assert.assertEquals("image/png", res.getHeader("Content-Type"));
+        Assert.assertEquals("Expires", "Wed, 21 Oct 2045 07:28:00 GMT", res.getHeader("Expires"));
+        Assert.assertEquals(null, res.getHeader("Last-Modified"));
+        Assert.assertEquals(null, res.getHeader("Content-Length"));
+        Assert.assertEquals(null, res.getHeader("Last-Modified"));
+    }
+
+    @Test
+    public void testReplaceAttributeValuesWithoutNull() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        String xml = "<a><b><c name=\"replace_this\">C</c></b></a>";
+        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+        doc = httpRequestService.replaceAttributeValues(doc, "//*[name()='c']", "name", "replace_this", "is_replaced");
+
+        NodeList elements = doc.getElementsByTagName("c");
+        NamedNodeMap attributes = elements.item(0).getAttributes();
+        Node attribute = attributes.getNamedItem("name");
+
+        Assert.assertEquals("is_replaced", attribute.getNodeValue());
+    }
+
+    @Test
+    public void testReplaceAttributeValuesWithNull() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        String xml = "<a><b><c name=\"replace_this\">C</c></b></a>";
+        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+        doc = httpRequestService.replaceAttributeValues(doc, "//*[name()='c']", "name", "replace_this", null);
+
+        NodeList elements = doc.getElementsByTagName("c");
+        NamedNodeMap attributes = elements.item(0).getAttributes();
+        Node attribute = attributes.getNamedItem("name");
+
+        Assert.assertEquals("replace_this", attribute.getNodeValue());
+    }
+
+    @Test
+    public void testDocumentToBytesArray() throws IOException, SAXException, ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        String xml = "<a><b>C</b></a>";
+        Document doc = builder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+        Assert.assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><a><b>C</b></a>",
+                new String(httpRequestService.documentToBytesArray(doc)));
+    }
+
+    @Test
+    public void testParseDocumentFromEntity() throws IOException, ParserConfigurationException, SAXException {
+        InputStream in = new ByteArrayInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><a><b>C</b></a>".getBytes());
+        HttpEntity entity = Mockito.mock(HttpEntity.class);
+        Mockito.when(entity.getContent()).thenReturn(in);
+
+        Document doc = httpRequestService.parseDocumentFromEntity(entity);
+
+        Assert.assertEquals("1.0", doc.getXmlVersion());
+    }
 }
