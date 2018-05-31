4a6b8e2e8df848d4ee9d9cfedf58aa53be3f8b47
Topi Penttinen <topi.penttinen@dimenteq.fi>
2018-05-31 14:10:30 +0300

    KSR-127 Add auth-headers into tests

--- src/test/java/fi/sitowise/ksr/controller/ProxyControllerTests.java
+++ src/test/java/fi/sitowise/ksr/controller/ProxyControllerTests.java
@@ -4,6 +4,7 @@ import fi.sitowise.ksr.domain.MapLayer;
 import fi.sitowise.ksr.service.MapLayerService;
 import fi.sitowise.ksr.service.ProxyService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -13,9 +14,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletResponse;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,6 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ComponentScan(basePackages =
         { "fi.sitowise.ksr.authentication", "fi.sitowise.ksr.config", "fi.sitowise.ksr.controller", "fi.sitowise.ksr.service" })
 public class ProxyControllerTests {
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Autowired
     ProxyController proxyController;
@@ -36,6 +43,12 @@ public class ProxyControllerTests {
 
     @MockBean
     MapLayerService mapLayerService;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity()).build();
+    }
 
     @Test
     public void testGeneralProxy() throws Exception {
@@ -52,8 +65,20 @@ public class ProxyControllerTests {
         ml.setUrl("http://test.example.com");
         Mockito.when(mapLayerService.getMapLayerById(Mockito.anyInt())).thenReturn(ml);
 
-        mockMvc.perform(get("/api/proxy/layer/134/1.00/GetCapalibites.xml")).andExpect(status().isOk());
-        mockMvc.perform(get("/api/proxy")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/api/proxy/layer/134/1.00/GetCapalibites.xml").header("OAM_REMOTE_USER", "TestUser")
+                .header("OAM_USER_FIRST_NAME", "firstName")
+                .header("OAM_USER_LAST_NAME", "lastName")
+                .header("OAM_USER_MAIL", "test@test.com")
+                .header("OAM_USER_MOBILE", "+123456789")
+                .header("OAM_ORGANIZATION", "sitowise")
+                .header("OAM_GROUPS", "KSR_ROLE_USER,KSR_ROLE_ADMIN")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/proxy").header("OAM_REMOTE_USER", "TestUser")
+                .header("OAM_USER_FIRST_NAME", "firstName")
+                .header("OAM_USER_LAST_NAME", "lastName")
+                .header("OAM_USER_MAIL", "test@test.com")
+                .header("OAM_USER_MOBILE", "+123456789")
+                .header("OAM_ORGANIZATION", "sitowise")
+                .header("OAM_GROUPS", "KSR_ROLE_USER,KSR_ROLE_ADMIN")).andExpect(status().isNotFound());
     }
 
     @Test
