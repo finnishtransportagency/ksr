690257df1fd33304a92eda207e0923999ee3d3e4
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-01 18:16:10 +0300

    KSR-126 Add MapLayers component

--- src/main/client/src/components/app/side-bar/SideBarView.js
+++ src/main/client/src/components/app/side-bar/SideBarView.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import SideBar from '../../ui/blocks/SideBar';
 import SearchView from './search/SearchView';
+import MapLayersContainer from './map-layers/MapLayersContainer';
 
 type Props = {
     activeNav: string,
@@ -10,7 +11,7 @@ type Props = {
 const SideBarView = ({ activeNav }: Props) => (
     <SideBar active={activeNav}>
         {activeNav === 'search' && <SearchView />}
-        {activeNav === 'mapLayers' && <h3>Karttatasot</h3>}
+        {activeNav === 'mapLayers' && <MapLayersContainer />}
         {activeNav === 'fileExport' && <h3>Tiedostojen vienti</h3>}
     </SideBar>
 );
