7f0e1014e4bd09bbab9cdfcea33d839c5d67d6b7
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-06-01 09:59:43 +0300

    KSR-22 Rework tests to use AuthControllerTestBase

--- src/test/java/fi/sitowise/ksr/controller/UserControllerTests.java
+++ src/test/java/fi/sitowise/ksr/controller/UserControllerTests.java
@@ -1,5 +1,6 @@
 package fi.sitowise.ksr.controller;
 
+import fi.sitowise.ksr.helper.AuthControllerTestBase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,46 +9,39 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * The type User controller tests.
+ */
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = {UserController.class})
 @ComponentScan(
         basePackages = {"fi.sitowise.ksr.authentication", "fi.sitowise.ksr.config"}
 )
-public class UserControllerTests {
+public class UserControllerTests extends AuthControllerTestBase {
 
-    @Autowired
-    private WebApplicationContext context;
-
-    @Autowired
-    private MockMvc mockMvc;
-
+    /**
+     * Sets .
+     */
     @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(springSecurity()).build();
-    }
+    public void setup() { init(); }
 
+    /**
+     * Test get user data.
+     *
+     * @throws Exception the exception
+     */
     @Test
     public void testGetUserData() throws Exception {
 
-        mockMvc.perform(get("/api/user")).andExpect(status().isForbidden());
+        this.mockMvc.perform(get("/api/user")).andExpect(status().isForbidden());
 
-        mockMvc.perform(get("/api/user")
-                .header("OAM_REMOTE_USER", "TestUser")
-                .header("OAM_USER_FIRST_NAME", "firstName")
-                .header("OAM_USER_LAST_NAME", "lastName")
-                .header("OAM_USER_MAIL", "test@test.com")
-                .header("OAM_USER_MOBILE", "+123456789")
-                .header("OAM_ORGANIZATION", "sitowise")
-                .header("OAM_GROUPS", "KSR_ROLE_USER,KSR_ROLE_ADMIN")).andExpect(status().isOk());
+        this.mockMvc.perform(get("/api/user")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_USER,KSR_ROLE_ADMIN"))).andExpect(status().isOk());
 
     }
 }
