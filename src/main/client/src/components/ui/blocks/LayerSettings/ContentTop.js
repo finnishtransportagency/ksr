11720b124b22f1604cacdc95ecaf8d37a0ce7fa3
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-04 17:31:21 +0300

    KSR-126 Add UI blocks for maplayer content

---
+++ src/main/client/src/components/ui/blocks/LayerSettings/ContentTop.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ContentTop = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+`;
+
+export default ContentTop;
