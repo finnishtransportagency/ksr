9168eb22b8935e589123ba386f3a215038c0a273
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-04 17:35:16 +0300

    KSR-126 Change MapLayers component structure
    - Now uses new MapLayersAll and MapLayersActive components

--- src/main/client/src/components/app/side-bar/map-layers/MapLayersContainer.js
+++ src/main/client/src/components/app/side-bar/map-layers/MapLayersContainer.js
@@ -1,15 +1,22 @@
 // @flow
 import { connect } from 'react-redux';
-import { getLayerGroups } from '../../../../reducers/map/actions';
+import { getLayerGroups, getActiveLayerTab, setActiveLayerTab } from '../../../../reducers/map/actions';
 import MapLayers from './MapLayers';
 
 const mapStateToProps = state => ({
     layerGroups: state.map.layerGroups,
+    activeTab: state.map.activeLayerTab,
 });
 
 const mapDispatchToProps = dispatch => ({
     getLayerGroups: () => {
         dispatch(getLayerGroups());
+    },
+    getActiveLayerTab: () => {
+        dispatch(getActiveLayerTab());
+    },
+    setActiveLayerTab: (tab) => {
+        dispatch(setActiveLayerTab(tab));
     },
 });
 
--- src/main/client/src/components/app/side-bar/map-layers/MapLayersView.js
+++ src/main/client/src/components/app/side-bar/map-layers/MapLayersView.js
@@ -1,25 +1,40 @@
 // @flow
 import React, { Fragment } from 'react';
 import SideBar from '../../../ui/blocks/SideBar';
-import { H1, H2, Button } from '../../../ui/elements';
-import { Wrapper } from './styles';
+import { H1 } from '../../../ui/elements';
+import MapLayersActiveContainer from './map-layers-active/MapLayersActiveContainer';
+import MapLayersAllContainer from './map-layers-all/MapLayersAllContainer';
+import { ButtonLayerNav } from './styles';
 
 type Props = {
-    layerGroups: Array<any>,
-}
+    handleButtonClickLayers: (string) => void,
+    activeTab: string,
+};
 
-const MapLayersView = ({ layerGroups }: Props) => (
-    <Wrapper>
+const MapLayersView = ({ handleButtonClickLayers, activeTab }: Props) => (
+    <Fragment>
         <SideBar.Header>
             <H1>Karttatasot</H1>
         </SideBar.Header>
         <SideBar.Content>
-            <Button disabled>Aktiiviset</Button>
-            <Button>Kaikki</Button>
-            <H2>{ layerGroups.map(lg => lg.name) }</H2>
-            { layerGroups.map(lg => lg.layers.map(l => <div key={l.id}><input checked={l.visible} type="checkbox" /><span>{l.name}</span></div>)) }
+            <ButtonLayerNav
+                flat
+                activeLayer={activeTab === 'active'}
+                onClick={() => handleButtonClickLayers('active')}
+            >
+                Aktiiviset
+            </ButtonLayerNav>
+            <ButtonLayerNav
+                flat
+                activeLayer={activeTab === 'all'}
+                onClick={() => handleButtonClickLayers('all')}
+            >
+                Kaikki
+            </ButtonLayerNav>
+            {activeTab === 'all' && <MapLayersAllContainer />}
+            {activeTab === 'active' && <MapLayersActiveContainer />}
         </SideBar.Content>
-    </Wrapper>
+    </Fragment>
 );
 
 export default MapLayersView;
