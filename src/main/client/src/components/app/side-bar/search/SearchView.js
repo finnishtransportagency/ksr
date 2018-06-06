4842a023afafc31f609a3f85d5d9cc52b80ca437
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-06 14:44:28 +0300

    KSR-126 Add string translation json

--- src/main/client/src/components/app/side-bar/search/SearchView.js
+++ src/main/client/src/components/app/side-bar/search/SearchView.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react';
+import strings from '../../../../translations';
 import { H1, Button } from '../../../ui/elements';
 import SideBar from '../../../ui/blocks/SideBar';
 
 const SearchView = () => (
     <Fragment>
         <SideBar.Header>
-            <H1>Haku</H1>
+            <H1>{strings.search.title}</H1>
         </SideBar.Header>
         <SideBar.Content>
-            <p>Lorem ipsum dolor sit amet.</p>
             <input type="text" />
-            <Button>Hae</Button>
+            <br />
+            <Button>{strings.search.buttonSearch}</Button>
         </SideBar.Content>
     </Fragment>
 );
---
+++ src/main/client/src/translations/fi.js
@@ -0,0 +1,13 @@
+const fi = {
+    mapLayers: {
+        title: 'Karttatasot',
+        active: 'Aktiiviset',
+        all: 'Kaikki',
+    },
+    search: {
+        title: 'Haku',
+        buttonSearch: 'Hae',
+    },
+};
+
+export default fi;
