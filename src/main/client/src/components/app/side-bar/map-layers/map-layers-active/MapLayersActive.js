3699674c49ace92ec76ff3fb6f7da9d47cb86f5f
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-12 11:28:10 +0300

    KSR-86 Add drag and drop logic

--- src/main/client/src/components/app/side-bar/map-layers/map-layers-active/MapLayersActive.js
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-active/MapLayersActive.js
@@ -9,25 +9,60 @@ type Props = {
     activeLayers: {
         activeLayers: Array<any>,
         fetching: boolean,
-    }
+    },
 };
 
 type State = {
     /* ... */
 };
 
+/*
+* Drag and drop stuff
+*/
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    background: isDragging ? '#444444' : 'transparent',
+    ...draggableStyle,
+});
+
+/*
+* Drag and drop stuff
+*/
+
 class MapLayersActive extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.onDragEnd = this.onDragEnd.bind(this);
+    }
+
     componentDidMount() {
         const { getActiveLayers } = this.props;
 
         getActiveLayers();
     }
 
+    onDragEnd = (result) => {
+        // dropped outside the list
+        console.log(result.destination);
+
+        if (!result.destination) {
+            return;
+        }
+    };
+
     render() {
         const { activeLayers } = this.props;
 
         if (!activeLayers.fetching) {
-            return <MapLayersActiveView activeLayers={activeLayers.activeLayers} />;
+            return (
+                <MapLayersActiveView
+                    activeLayers={activeLayers.activeLayers}
+                    getItemStyle={getItemStyle}
+                    onDragEnd={this.onDragEnd}
+                />
+            );
         }
 
         return <LoadingIcon loading={activeLayers.fetching} />;
