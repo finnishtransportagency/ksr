4071bde9eff0275c7891ec4d4988fed4ecb603c3
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-06-15 12:24:58 +0300

    KSR-42 Add translations to reactTable

--- src/main/client/src/translations/fi.js
+++ src/main/client/src/translations/fi.js
@@ -8,6 +8,15 @@ const fi = {
         title: 'Haku',
         buttonSearch: 'Hae',
     },
+    reactTable: {
+        previousText: 'Edellinen',
+        nextText: 'Seuraava',
+        loadingText: 'Ladataan...',
+        noDataText: 'Rivejä ei löytynyt',
+        pageText: 'Sivu',
+        ofText: '/',
+        rowsText: 'riviä',
+    },
 };
 
 export default fi;
