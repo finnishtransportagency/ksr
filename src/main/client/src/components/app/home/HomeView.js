6c4cf49866ea8cd92954608d29c493a8ee254f36
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-06-15 12:27:27 +0300

    KSR-42 Add TableContainer to HomeView

--- src/main/client/src/components/app/home/HomeView.js
+++ src/main/client/src/components/app/home/HomeView.js
@@ -3,12 +3,14 @@ import React, { Fragment } from 'react';
 import EsriMapContainer from '../map/EsriMapContainer';
 import SideBarContainer from '../side-bar/SideBarContainer';
 import SideNavContainer from '../side-nav/SideNavContainer';
+import TableContainer from '../table/TableContainer';
 
 const HomeView = () => (
     <Fragment>
         <SideNavContainer />
         <SideBarContainer />
         <EsriMapContainer />
+        <TableContainer />
     </Fragment>
 );
 
