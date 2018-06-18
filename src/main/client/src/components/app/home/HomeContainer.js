ec7a2073096f0427c9b705935a909a3ca5f6be99
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-06-18 15:41:17 +0300

    KSR-156 Fetch map config when application loads

--- src/main/client/src/components/app/home/HomeContainer.js
+++ src/main/client/src/components/app/home/HomeContainer.js
@@ -1,11 +1,14 @@
 // @flow
 import { connect } from 'react-redux';
-import { getLayerGroups } from '../../../reducers/map/actions';
+import { getLayerGroups, getMapConfig } from '../../../reducers/map/actions';
 import Home from './Home';
 
 const mapDispatchToProps = dispatch => ({
     getLayerGroups: () => {
         dispatch(getLayerGroups());
+    },
+    getMapConfig: () => {
+        dispatch(getMapConfig());
     },
 });
 
