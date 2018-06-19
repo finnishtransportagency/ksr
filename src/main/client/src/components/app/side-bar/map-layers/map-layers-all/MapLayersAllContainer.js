6a3de6c722f9c0c86ff498ce23e03a01bfdb8776
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-19 17:21:02 +0300

    KSR-160 Add new redux props to component

--- src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAllContainer.js
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAllContainer.js
@@ -1,15 +1,20 @@
 // @flow
 import { connect } from 'react-redux';
-import { getLayerGroups } from '../../../../../reducers/map/actions';
+import { getLayerGroups, setLayerList } from '../../../../../reducers/map/actions';
 import MapLayersAll from './MapLayersAll';
 
 const mapStateToProps = state => ({
-    layerGroups: state.map.layerGroups,
+    layerGroups: state.map.layerGroups.layerGroups,
+    layerList: state.map.layerGroups.layerList,
+    fetching: state.map.layerGroups.fetching,
 });
 
 const mapDispatchToProps = dispatch => ({
     getLayerGroups: () => {
         dispatch(getLayerGroups());
+    },
+    setLayerList: (layerList) => {
+        dispatch(setLayerList(layerList));
     },
 });
 
