d7bae9d3ee9086c1a06ca7b8e8535f00fd82be3e
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-06-20 12:35:30 +0300

    KSR-160 Fix layersActive unit tests

--- src/main/client/src/components/app/side-bar/map-layers/map-layers-active/__tests__/MapLayersActiveView.test.js
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-active/__tests__/MapLayersActiveView.test.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { Draggable, DragDropContext, Droppable } from 'react-beautiful-dnd';
 import LayerSettings from '../../../../../ui/blocks/LayerSettings';
 import MapLayersActiveView from '../MapLayersActiveView';
 
 const setup = () => {
     const props = {
-        activeLayers: [
+        layerList: [
             {
                 id: 1,
-                name: 'test',
+                active: false,
+                name: 'Layer 1',
+            },
+            {
+                id: 2,
+                active: true,
+                name: 'Layer 2',
+            },
+            {
+                id: 3,
+                active: false,
+                name: 'Layer 3',
+            },
+            {
+                id: 4,
+                active: true,
+                name: 'Layer 4',
+            },
+            {
+                id: 5,
+                active: true,
+                name: 'Layer 5',
             },
         ],
         onDragEnd: () => {},
     };
-    const wrapper = shallow(<MapLayersActiveView {...props} />);
+    const wrapper = mount(<MapLayersActiveView {...props} />);
 
     return { props, wrapper };
 };
@@ -26,8 +47,9 @@ describe('<MapLayersActiveView />', () => {
         expect(wrapper.find(DragDropContext).length).toBe(1);
     });
 
-    it('should render Droppable element', () => {
-        const dragDrop = wrapper.find(DragDropContext);
-        expect(dragDrop.find(Droppable).length).toBe(1);
+    it('should render active layers only', () => {
+        expect(wrapper.find(Droppable).exists()).toBe(true);
+        expect(wrapper.find(Draggable).exists()).toBe(true);
+        expect(wrapper.find(LayerSettings).length).toBe(3);
     });
 });
