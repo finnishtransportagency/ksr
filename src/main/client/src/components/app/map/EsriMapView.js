2a15cd7d0e0c6266031fef6743d6dfbac257878f
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-06-29 14:19:35 +0300

    KSR-31 Changes to EsriMap
    MapView reorder needed special handling for
    displaying sketchViewModel.

--- src/main/client/src/components/app/map/EsriMapView.js
+++ src/main/client/src/components/app/map/EsriMapView.js
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react';
 import MapMeasure from './map-measure/MapMeasure';
+import SketchToolContainer from './sketch-tools/SketchToolContainer';
+
 import { Wrapper } from './styles';
 
 type Props = {
@@ -13,6 +15,7 @@ const EsriMapView = ({ activeNav, view, isOpenTable }: Props) => (
     <Wrapper sideBar={activeNav} toggleTable={isOpenTable}>
         <div id="mapView">
             <MapMeasure view={view} />
+            <SketchToolContainer view={view} />
         </div>
     </Wrapper>
 );
