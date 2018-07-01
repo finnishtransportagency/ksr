bfb47cd6f03beca682abb8a1cbc3e9a9e45ccda2
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-07-01 22:27:33 +0300

    KSR-164 Add unit test for searchState reducer

---
+++ src/main/client/src/reducers/search/__tests__/searchState.test.js
@@ -0,0 +1,102 @@
+import * as types from '../../../constants/actionTypes';
+import { searchFeatures } from '../../table/actions';
+import { setSearchState } from '../actions';
+import reducer from '../searchState';
+
+const initialState = {
+    selectedLayer: 0,
+    textSearch: '',
+    searchFieldValues: [],
+    optionsField: [],
+    optionsExpression: [
+        {
+            value: '%',
+            label: '%',
+        },
+        {
+            value: '<=',
+            label: '<=',
+        },
+        {
+            value: '>=',
+            label: '>=',
+        },
+        {
+            value: '=',
+            label: '=',
+        },
+        {
+            value: '!=',
+            label: '!=',
+        },
+    ],
+    fetching: false,
+};
+
+describe('Search state reducer', () => {
+    it('should return initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should handle SEARCH_FEATURES', () => {
+        expect(reducer(undefined, {
+            type: types.SEARCH_FEATURES,
+        })).toEqual({ ...initialState, fetching: true });
+    });
+
+    it('should handle SEARCH_FEATURES_FULFILLED', () => {
+        expect(reducer(undefined, {
+            type: types.SEARCH_FEATURES_FULFILLED,
+        })).toEqual({ ...initialState, fetching: false });
+    });
+
+    it('should handle SET_LAYER_LIST', () => {
+        expect(reducer(undefined, {
+            type: types.SET_LAYER_LIST,
+            queryableLayers: [],
+        })).toEqual({ ...initialState });
+    });
+
+    it('should handle SET_SEARCH_STATE', () => {
+        const action = {
+            selectedLayer: 1,
+            textSearch: '',
+            searchFieldValues: [
+                {
+                    id: 1,
+                    name: 'name',
+                    queryExpression: '=',
+                    queryText: 'helsinki',
+                },
+            ],
+        };
+
+        expect(reducer(undefined, {
+            type: types.SET_SEARCH_STATE,
+            selectedLayer: action.selectedLayer,
+            textSearch: action.textSearch,
+            searchFieldValues: action.searchFieldValues,
+        })).toEqual({
+            ...initialState,
+            selectedLayer: action.selectedLayer,
+            textSearch: action.textSearch,
+            searchFieldValues: action.searchFieldValues,
+        });
+    });
+
+    it('should handle SET_SEARCH_OPTIONS', () => {
+        const action = {
+            optionsField: [
+                {
+                    value: 1,
+                    name: 'field 1',
+                },
+            ],
+        };
+
+        expect(reducer(undefined, {
+            type: types.SET_SEARCH_OPTIONS,
+            optionsField: action.optionsField,
+        })).toEqual({ ...initialState, optionsField: action.optionsField });
+    });
+});
