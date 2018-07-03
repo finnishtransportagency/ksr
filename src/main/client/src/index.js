9510d745ed7f7862101d2d1b491cf3ca24130c30
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-07-03 09:45:00 +0300

    KSR-38 Use application locale for Esri library

--- src/main/client/src/index.js
+++ src/main/client/src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import strings from './translations';
+
+window.dojoConfig = {
+    locale: strings.getLanguage(),
+};
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
