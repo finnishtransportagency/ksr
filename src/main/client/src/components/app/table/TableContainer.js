edf48ae3cc067e68f81b81a919bc0ab1099fc907
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-07-25 23:19:52 +0300

    KSR-24 Filter out all data keys that starts with underscore
    - Easier to do original and edited layers comparison if only contains actual field data

--- src/main/client/src/components/app/table/TableContainer.js
+++ src/main/client/src/components/app/table/TableContainer.js
@@ -4,12 +4,38 @@ import { toggleFilter, toggleTable } from '../../../reducers/table/actions';
 import { setActiveModal } from '../../../reducers/modal/actions';
 import TableView from './TableView';
 
-const mapStateToProps = state => ({
-    isOpen: state.table.toggleTable,
-    isOpenFilter: state.table.toggleFilter,
-    activeNav: state.navigation.activeNav,
-    activeModal: state.modal.activeModal,
-});
+const mapStateToProps = (state) => {
+    const removeUnderscore = (layer) => {
+        const filteredData = [];
+        layer.data.map((d) => {
+            const filtered = Object.keys(d)
+                .filter(key => key.substring(0, 1) !== '_')
+                .reduce((obj, key) => {
+                    obj[key] = d[key];
+                    return obj;
+                }, {});
+
+            return filteredData.push(filtered);
+        });
+
+        return {
+            ...layer,
+            data: filteredData,
+        };
+    };
+
+    const editedLayers = state.table.features.editedLayers.map(l => removeUnderscore(l));
+    const originalLayers = state.table.features.layers.map(l => removeUnderscore(l));
+
+    return {
+        isOpen: state.table.toggleTable,
+        isOpenFilter: state.table.toggleFilter,
+        activeNav: state.navigation.activeNav,
+        activeModal: state.modal.activeModal,
+        originalLayers,
+        editedLayers,
+    };
+};
 
 const mapDispatchToProps = dispatch => ({
     toggleTable: () => {
