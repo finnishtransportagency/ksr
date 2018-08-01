6b64e5b18004ec194bb7b27e1c0f572adac3cf38
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-08-01 12:46:10 +0300

    KSR-26 Add textarea and table to delete modal

--- src/main/client/src/components/app/table/modal-delete-selected/ModalDeleteSelected.js
+++ src/main/client/src/components/app/table/modal-delete-selected/ModalDeleteSelected.js
@@ -1,23 +1,57 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import strings from '../../../../translations';
 import ModalContainer from '../../shared/Modal/ModalContainer';
+import ModalDeleteSelectedView from './ModalDeleteSelectedView';
+import { TextArea, Text } from './styles';
 
 type Props = {
     selectedData: Array<Object>,
-    deleteSelectedData: (selectedData: Array<Object>) => void,
+    filteredData: Array<Object>,
+    deleteSelectedData: (selectedData: Array<Object>, deleteComment: string) => void,
 };
 
-const ModalDeleteSelected = ({ selectedData, deleteSelectedData }: Props) => (
-    <ModalContainer
-        title={strings.modalDeleteSelected.title}
-        handleModalSubmit={() => { deleteSelectedData(selectedData); }}
-        submitText={strings.modalDeleteSelected.submit}
-        cancelText={strings.modalDeleteSelected.cancel}
-    >
-        {strings.modalDeleteSelected.content}
-        <p>{ selectedData && selectedData.map(l => `${l._id} `)}</p>
-    </ModalContainer>
-);
+type State = {
+    deleteComment: string,
+};
+
+const initialState = {
+    deleteComment: '',
+};
+
+class ModalDeleteSelected extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = { ...initialState };
+
+        this.handleTextareaChange = this.handleTextareaChange.bind(this);
+    }
+
+    handleTextareaChange = (e: Object) => {
+        this.setState({ deleteComment: e.target.value });
+    };
+
+    render() {
+        const { selectedData, filteredData, deleteSelectedData } = this.props;
+        const { deleteComment } = this.state;
+
+        return (
+            <ModalContainer
+                title={strings.modalDeleteSelected.title}
+                handleModalSubmit={() => { deleteSelectedData(selectedData, deleteComment); }}
+                submitText={strings.modalDeleteSelected.submit}
+                cancelText={strings.modalDeleteSelected.cancel}
+            >
+                <ModalDeleteSelectedView
+                    selectedData={selectedData}
+                    deleteComment={deleteComment}
+                    handleTextareaChange={this.handleTextareaChange}
+                    filteredData={filteredData}
+                />
+            </ModalContainer>
+        );
+    }
+}
 
 export default ModalDeleteSelected;
---
+++ src/main/client/src/components/app/table/modal-delete-selected/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import * as styles from '../../../ui/defaultStyles';
+
+export const TextArea = styled.textarea`
+    min-width: 100%;
+    max-width: 100%;
+    min-height: 4rem;
+    box-sizing: border-box;
+    padding: 0.5rem;
+    border: 1px solid ${styles.colorBackgroundGrey};
+    
+    &:focus {
+        outline: none;
+        border: 1px solid ${styles.colorMain};
+    };
+`;
+
+export const FilteredDataTable = styled.table`
+    text-align: left;
+    display: block;
+    overflow: auto;
+    
+    th, td {
+        padding-right: 1rem;
+    }
+    
+    th:last-of-type, td:last-of-type {
+        padding: 0;
+    }
+`;
