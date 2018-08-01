4090b1e22a3b5bb0bdb6bd431d42ef99e12efed0
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-08-01 15:26:52 +0300

    KSR-26 Add unit tests for shared modal

---
+++ src/main/client/src/components/app/shared/Modal/__tests__/Modal.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Modal from '../Modal';
+
+const setup = (prop) => {
+    const minProps = {
+        title: 'Modal Title',
+        submitText: 'Submit',
+        cancelText: 'Cancel',
+        handleModalSubmit: jest.fn(),
+        activeModal: '',
+        setActiveModal: () => {},
+    };
+    const props = prop || minProps;
+    const wrapper = shallow(<Modal {...props}><div>Children</div></Modal>);
+
+    return { props, wrapper };
+};
+
+describe('<Modal />', () => {
+    const { wrapper } = setup();
+
+    it('should render self', () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it('should toggleModal properly', () => {
+        const { activeModal } = wrapper.instance().props;
+
+        wrapper.instance().toggleModal();
+        expect(activeModal).toBe('');
+    });
+
+    it('should handle submitting properly', () => {
+        const { handleModalSubmit } = wrapper.instance().props;
+        const spyToggleModal = jest.spyOn(wrapper.instance(), 'toggleModal');
+
+        wrapper.instance().handleSubmit();
+
+        expect(handleModalSubmit).toHaveBeenCalled();
+        expect(spyToggleModal).toHaveBeenCalled();
+    });
+});
