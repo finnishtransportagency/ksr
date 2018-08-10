9014933c99fd5db9b5a6055b53562f435b7109d5
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-08-10 15:32:20 +0300

    KSR-40 Add new controllers for print

---
+++ src/main/java/fi/sitowise/ksr/controller/PrintOutputController.java
@@ -0,0 +1,44 @@
+package fi.sitowise.ksr.controller;
+
+import fi.sitowise.ksr.service.PrintService;
+import fi.sitowise.ksr.utils.KsrRequestUtils;
+import fi.sitowise.ksr.utils.KsrStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.regex.Pattern;
+
+@RestController
+@RequestMapping(PrintOutputController.PRINT_OUTPUT_URL)
+public class PrintOutputController {
+
+    private final PrintService printService;
+    public static final String PRINT_OUTPUT_URL = "/api/print/output";
+    private Pattern printOutputProxyUrlPattern;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    public PrintOutputController(PrintService printService) {
+        this.printService = printService;
+    }
+
+    @PostConstruct
+    public void setUpPrintProxyUrlMatcher() {
+        String patternToMatch = "^" + contextPath + "/api/print/output/(.*?)$";
+        printOutputProxyUrlPattern = Pattern.compile(KsrStringUtils.replaceMultipleSlashes(patternToMatch));
+    }
+
+    @RequestMapping(value = "/**", method = { RequestMethod.GET })
+    public void printOutputProxy(HttpServletRequest request, HttpServletResponse response) {
+        String serviceEndpoint = KsrRequestUtils.getServiceEndpoint(printOutputProxyUrlPattern, request.getRequestURI());
+        printService.getPrintOutput(serviceEndpoint, request, response);
+    }
+}
