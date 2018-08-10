a5ab94a22495188c0d0394b45728d6168bcdd946
Topi Penttinen <topi.penttinen@sitowise.com>
2018-08-10 13:50:01 +0300

    KSR-170 Add enumeration for layer-roles

---
+++ src/main/java/fi/sitowise/ksr/domain/LayerAction.java
@@ -0,0 +1,11 @@
+package fi.sitowise.ksr.domain;
+
+/**
+ * Enumeration helper holding possible action-types for layers.
+ */
+public enum LayerAction {
+    CREATE_LAYER, // Create features on layer
+    READ_LAYER, // Read features on layer
+    UPDATE_LAYER, // Update features on layer
+    DELETE_LAYER, // Delete features on layer
+}
