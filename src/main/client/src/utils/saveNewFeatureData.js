5494a45cd46c9f5e445eeb9f5119d8e65ca2c580
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-08-20 10:35:04 +0300

    KSR-49 Add new helper utils
    -GeometryType finder
    -Parse column and geometry type
    -Save new feature data skeleton

---
+++ src/main/client/src/utils/saveNewFeatureData.js
@@ -0,0 +1,12 @@
+// @flow
+/**
+ * TODO: add save to argcis server
+ * This operation adds features to the associated feature layer
+ * @param data Object feature layer
+ */
+
+export const saveNewFeatureData = (data: Object) => {
+    const arrayData = [];
+    arrayData.push(data);
+    console.log(arrayData);
+};
