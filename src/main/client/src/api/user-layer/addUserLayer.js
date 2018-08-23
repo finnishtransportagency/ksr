e688c6cb5f8893a6b1497c39746f2ff602730084
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-08-23 16:42:25 +0300

    KSR-189 Add fetch API for adding new user layer

---
+++ src/main/client/src/api/user-layer/addUserLayer.js
@@ -0,0 +1,7 @@
+import { getHeaders } from '../config';
+
+export const fetchAddUserLayer = data => fetch('api/user-layer', {
+    headers: getHeaders(),
+    method: 'POST',
+    body: JSON.stringify(data),
+});
