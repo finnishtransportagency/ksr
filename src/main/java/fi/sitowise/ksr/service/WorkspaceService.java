7485650a5da497a776ae9ff17a19c4e74396a06b
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-09-03 13:11:55 +0300

    KSR-197 Create service for saving workspace

---
+++ src/main/java/fi/sitowise/ksr/service/WorkspaceService.java
@@ -0,0 +1,34 @@
+package fi.sitowise.ksr.service;
+
+import fi.sitowise.ksr.domain.Workspace;
+import fi.sitowise.ksr.repository.WorkspaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Workspace service.
+ */
+@Service
+public class WorkspaceService {
+    private final WorkspaceRepository workspaceRepository;
+
+    /**
+     * Instantiates new workspace service.
+     *
+     * @param workspaceRepository workspace repository
+     */
+    @Autowired
+    public WorkspaceService(WorkspaceRepository workspaceRepository) {
+        this.workspaceRepository = workspaceRepository;
+    }
+
+    /**
+     * Save new workspace to database.
+     *
+     * @param workspace workspace to be saved
+     * @param username username of the user whom the workspace is saved for
+     */
+    public void saveWorkspace(Workspace workspace, String username) {
+        workspaceRepository.saveWorkspace(workspace, username);
+    }
+}
---
+++ src/test/java/fi/sitowise/ksr/service/WorkspaceServiceTests.java
@@ -0,0 +1,43 @@
+package fi.sitowise.ksr.service;
+
+import fi.sitowise.ksr.domain.Workspace;
+import fi.sitowise.ksr.repository.WorkspaceRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+/**
+ * Workspace service tests.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = SpringRunner.class)
+@ContextConfiguration(classes = WorkspaceService.class)
+public class WorkspaceServiceTests {
+
+    @Autowired
+    WorkspaceService workspaceService;
+
+    @MockBean
+    WorkspaceRepository workspaceRepository;
+
+    /**
+     * Test add workspace.
+     */
+    @Test
+    public void testSaveWorkspaceOk() {
+        Workspace workspace = new Workspace();
+        workspace.setName("test workspace");
+        workspace.setScale(2000);
+        workspace.setCenterLongitude(100);
+        workspace.setCenterLatitude(100);
+        workspace.setLayers(new ArrayList<>());
+
+        workspaceService.saveWorkspace(workspace, "test-user");
+    }
+}
