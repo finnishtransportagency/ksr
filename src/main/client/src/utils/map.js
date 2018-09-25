d14c61b365326f28a665b7ccdceaabbc539100f6
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-09-25 15:50:38 +0300

    KSR-215 Add util for changing maps center point
    - Changed addLayers method to return promise

--- src/main/client/src/utils/map.js
+++ src/main/client/src/utils/map.js
@@ -22,6 +22,42 @@ export const fitExtent = (layers: Array<Object>, view: Object) => {
 };
 
 /**
+ * Fit map to given scale and center point
+ *
+ * @param mapCenter array of x and y coordinates
+ * @param mapScale scale of mapview
+ * @param view esri/views/MapView
+ */
+export const setCenterPoint = (
+    mapCenter: Array<any>,
+    mapScale: number,
+    view: Object,
+) => {
+    esriLoader
+        .loadModules([
+            'esri/geometry/Point',
+            'esri/geometry/SpatialReference',
+        ])
+        .then(([
+            Point,
+            SpatialReference,
+        ]) => {
+            const epsg3067 = new SpatialReference(3067);
+
+            const point = Point({
+                x: mapCenter[0],
+                y: mapCenter[1],
+                spatialReference: epsg3067,
+            });
+
+            view.goTo({
+                target: point,
+                scale: mapScale,
+            });
+        });
+};
+
+/**
  * Add new layers to map view.
  * If search layers are present or added also fit map on the extent of search layers.
  *
@@ -29,21 +65,25 @@ export const fitExtent = (layers: Array<Object>, view: Object) => {
  * @param view Map view to which the layers are added
  * @param searchLayers Array of search layers that already exist in map view
  */
-export const addLayers = (layers: Array<Object>, view: Object, searchLayers: Array<Object>) => {
-    esriLoader
-        .loadModules([
-            'esri/config',
-            'esri/layers/WMSLayer',
-            'esri/layers/WMTSLayer',
-            'esri/layers/FeatureLayer',
-        ])
-        .then(([
-            esriConfig,
-            WMSLayer,
-            WMTSLayer,
-            FeatureLayer,
-        ]) => {
-            layers.forEach((layer) => {
+export const addLayers = (
+    layers: Array<Object>,
+    view: Object,
+    searchLayers: Array<Object>,
+) => esriLoader
+    .loadModules([
+        'esri/config',
+        'esri/layers/WMSLayer',
+        'esri/layers/WMTSLayer',
+        'esri/layers/FeatureLayer',
+    ])
+    .then(([
+        esriConfig,
+        WMSLayer,
+        WMTSLayer,
+        FeatureLayer,
+    ]) => {
+        layers.forEach((layer) => {
+            if (layer.active && !view.map.layers.find(l => l.id === layer.id)) {
                 esriConfig.request.corsEnabledServers.push(layer.url);
                 switch (layer.type) {
                     // sublayers split will work as long as name doesn't contain comma
@@ -99,11 +139,9 @@ export const addLayers = (layers: Array<Object>, view: Object, searchLayers: Arr
                     default:
                         break;
                 }
-            });
-
-            if (searchLayers.length) fitExtent(searchLayers, view);
+            }
         });
-};
+    });
 
 /**
 * Creates an esri.Graphic for highlight purposes from given geometry.
@@ -213,7 +251,7 @@ export const highlight = (
                     const { layer } = lv;
                     if (layer.visible && layer.queryFeatures) {
                         const ids = selectedFeatures
-                            .filter(f => f._layerId === layer.id)
+                            .filter(f => f._layerId.startsWith(layer.id))
                             .map(f => parseInt(f._id, 10));
                         if (activeAdminTool && activeAdminTool === layer.id) {
                             if (layer.featureType === 'shapefile') {
