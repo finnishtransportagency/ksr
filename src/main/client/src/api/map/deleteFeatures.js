717babef9ff2a89fe2960ebe2784e688fc91dcb2
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-10-08 13:30:43 +0300

    KSR-230 Add api call for feature deletion

---
+++ src/main/client/src/api/map/deleteFeatures.js
@@ -0,0 +1,21 @@
+// @flow
+
+/**
+ * Calls arcGIS Server's deleteFeatures endpoint.
+ *
+ * @param {string} layerId Id of the corresponding layer.
+ * @param {string} params Parameters to be sent in the request.
+ *
+ * @returns {Promise} Promise with json body.
+ */
+export const deleteFeatures = (layerId: string, params: string) =>
+    fetch(`api/proxy/layer/${layerId}/deleteFeatures`, {
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        method: 'POST',
+        body: params,
+    })
+        .then(r => r.json())
+        .catch(err => console.error(err));
