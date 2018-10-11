81d0abd0c7c194852e77517738bf9b312e8445bf
Topi Penttinen <topi.penttinen@sitowise.com>
2018-10-11 09:52:54 +0300

    KSR-18 Create new component DataLayersActiveView

---
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-active/data-layers-active/DataLayersActiveView.js
@@ -0,0 +1,40 @@
+// @flow
+import React, { Fragment } from 'react';
+import MapLayerSettings from '../map-layer-settings/MapLayerSettings';
+
+type Props = {
+    dataLayerList: Object[],
+    setActiveAdminTool: (layerId: string, layerList: Array<any>) => void,
+    createNonSpatialFeature: () => void,
+    activeAdminTool: string,
+};
+
+const DataLayersActiveView = ({
+    dataLayerList,
+    setActiveAdminTool,
+    activeAdminTool,
+    createNonSpatialFeature,
+}: Props) => {
+    if (dataLayerList.length === 0) return null;
+    return (
+        <Fragment>
+            <br />
+            {
+                dataLayerList.map(l => (
+                    <MapLayerSettings
+                        key={l.id}
+                        layer={l}
+                        layerList={dataLayerList}
+                        setActiveAdminTool={setActiveAdminTool}
+                        activeAdminTool={activeAdminTool}
+                        createNonSpatialFeature={createNonSpatialFeature}
+                        onOpacityChange={() => {}}
+                        onToggleVisibility={() => {}}
+                    />
+                ))
+            }
+        </Fragment>
+    );
+};
+
+export default DataLayersActiveView;
