28115ad87920070a17d6aeed84de05f70caf46a2
Topi Penttinen <topi.penttinen@sitowise.com>
2018-10-15 09:18:47 +0300

    KSR-68 Add new method to return updater name

--- src/main/java/fi/sitowise/ksr/utils/KsrAuthenticationUtils.java
+++ src/main/java/fi/sitowise/ksr/utils/KsrAuthenticationUtils.java
@@ -1,5 +1,8 @@
 package fi.sitowise.ksr.utils;
 
+import fi.sitowise.ksr.authentication.User;
+import fi.sitowise.ksr.exceptions.KsrApiException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -24,5 +27,30 @@ public class KsrAuthenticationUtils {
      */
     public static String getCurrentUsername() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    /**
+     * Get current user's name combined from first letter of firstname + lastname.
+     * E.g. Matti Meikalainen  -&gt; M Meikalainen.
+     *
+     * @return first letter of firstname + lastname.
+     */
+    public static String getCurrentUserUpdaterName() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (user.getFirstName() == null || user.getFirstName().isEmpty()
+                || user.getLastName() == null || user.getLastName().isEmpty()) {
+            throw new KsrApiException.ForbiddenException(
+                    String.format(
+                            "No \"%s\" defined for user: %s",
+                            user.getFirstName() == null || user.getFirstName().isEmpty() ? "firstName" : "lastName",
+                            user.getUsername()
+                    )
+            );
+        }
+        return String.format(
+                "%s %s",
+                user.getFirstName().substring(0, 1).toUpperCase(),
+                StringUtils.capitalize(user.getLastName().toLowerCase())
+        );
     }
 }
