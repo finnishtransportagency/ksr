0b90544966048b4067ce22052488532b0abfa5dd
Riku Rohkea <riku.rohkea@dimenteq.fi>
2018-11-12 16:08:12 +0200

    KSR-267 Enable Spring pre/post annotations

--- src/main/java/fi/sitowise/ksr/config/SecurityConfig.java
+++ src/main/java/fi/sitowise/ksr/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package fi.sitowise.ksr.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import fi.sitowise.ksr.authentication.OAMFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final OAMAuthenticationProvider oamAuthenticationProvider;
 
