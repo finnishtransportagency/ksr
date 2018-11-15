5282ab82d4de12dc92adb4692a3908d5811cfbdd
Topi Penttinen <topi.penttinen@sitowise.com>
2018-11-15 10:22:21 +0200

    KSR-260 Remove ServiceWorker

--- src/main/client/src/index.js
+++ src/main/client/src/index.js
@@ -2,11 +2,11 @@ import 'babel-polyfill';
 import 'url-search-params-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TextDecoder } from 'text-encoding';
+
 import App from './components/app/App';
-import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import strings from './translations';
-import { TextDecoder } from 'text-encoding';
 
 if (!window.TextDecoder) window.TextDecoder = TextDecoder;
 
@@ -15,4 +15,3 @@ window.dojoConfig = {
 };
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
