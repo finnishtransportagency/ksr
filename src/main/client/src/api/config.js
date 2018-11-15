7931ca99154032ac4bee235eaa5c1dd275714fd2
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-11-15 17:34:01 +0200

    KSR-259 Add error handler for fetch

--- src/main/client/src/api/config.js
+++ src/main/client/src/api/config.js
@@ -1,3 +1,5 @@
+import { toast } from 'react-toastify';
+
 export const getHeaders = () => {
     const headers = new Headers();
     headers.append('X-Requested-With', 'XMLHttpRequest');
@@ -9,3 +11,11 @@ export const config = () => ({
     headers: getHeaders(),
     credentials: 'include',
 });
+
+export const handleErrors = (response, toastText) => {
+    if (!response.ok) {
+        if (toastText) toast.error(toastText);
+        throw Error(response.statusText);
+    }
+    return response;
+};
