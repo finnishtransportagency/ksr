e6d14ccf15931535bcdfb5e7af3038a9fe4664a9
Markus Åfelt <markus.afelt@dimenteq.fi>
2018-11-22 09:28:01 +0200

    KSR-92 Remove unnecessary property

--- src/main/client/src/components/app/map/InitMapContainer.js
+++ src/main/client/src/components/app/map/InitMapContainer.js
@@ -7,24 +7,18 @@ import { setSingleLayerGeometry, searchWorkspaceFeatures, selectFeatures } from 
 import { setWorkspace, setWorkspaceRejected } from '../../../reducers/workspace/actions';
 import InitMap from './InitMap';
 
-const mapStateToProps = (state) => {
-    const selectedFeatures = state.table.features.layers
-        .reduce((a, b) => a.concat(b.data.filter(d => d._selected)), []);
-
-    return ({
-        layerList: state.map.layerGroups.layerList,
-        mapCenter: state.map.mapConfig.mapCenter,
-        mapScale: state.map.mapConfig.mapScale,
-        printServiceUrl: state.map.mapConfig.printServiceUrl,
-        selectedFeatures,
-        activeAdminTool: state.adminTool.active.layerId,
-        sketchViewModel: state.map.mapTools.sketchViewModel,
-        editMode: state.map.mapTools.editMode,
-        activeTool: state.map.mapTools.active,
-        initialLoading: state.map.mapConfig.fetching || state.map.layerGroups.fetching,
-        authorities: state.user.userInfo.authorities,
-    });
-};
+const mapStateToProps = state => ({
+    layerList: state.map.layerGroups.layerList,
+    mapCenter: state.map.mapConfig.mapCenter,
+    mapScale: state.map.mapConfig.mapScale,
+    printServiceUrl: state.map.mapConfig.printServiceUrl,
+    activeAdminTool: state.adminTool.active.layerId,
+    sketchViewModel: state.map.mapTools.sketchViewModel,
+    editMode: state.map.mapTools.editMode,
+    activeTool: state.map.mapTools.active,
+    initialLoading: state.map.mapConfig.fetching || state.map.layerGroups.fetching,
+    authorities: state.user.userInfo.authorities,
+});
 
 const mapDispatchToProps = dispatch => ({
     selectFeatures: (features) => {
