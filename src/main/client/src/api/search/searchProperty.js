590b0ebdaa7f349b12aedee16e3c6556e265b83e
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-11-28 18:30:22 +0200

    KSR-285 Add signal and polygon support to property search

--- src/main/client/src/api/search/searchProperty.js
+++ src/main/client/src/api/search/searchProperty.js
@@ -8,18 +8,19 @@ import { config, handleErrors } from '../config';
  * Finds property info with X and Y coordinates or property identifier.
  *
  * @param {Object | string} queryParameter Parameters to be used in query.
- * Can be either X and Y coordinates or property identifier.
+ * Can be either X and Y coordinates, string made from area geometry or property identifier.
+ * @param {any} signal Abortcontroller signal.
  *
  * @returns {Promise} Promise with property info.
  */
-export const fetchPropertyInfo = (queryParameter: any): Object => {
+export const fetchPropertyInfo = (queryParameter: any, signal: any): Object => {
     if (queryParameter.x && queryParameter.y) {
         return fetch(`api/property/?${
             querystring.stringify({
                 x: queryParameter.x,
                 y: queryParameter.y,
             })
-        }`, config())
+        }`, { ...config(), signal })
             .then(res => handleErrors(res, strings.searchProperty.errorToast.searchFailed))
             .then(res => res.json())
             .then((res) => {
@@ -27,11 +28,25 @@ export const fetchPropertyInfo = (queryParameter: any): Object => {
                     toast.error(strings.searchProperty.errorToast.searchCoordsNoResults);
                 }
                 return res;
-            })
-            .catch(err => console.log(err));
+            });
+    } else if (queryParameter.polygon) {
+        return fetch('api/property/', {
+            ...config(),
+            method: 'POST',
+            body: JSON.stringify({ polygon: queryParameter.polygon }),
+            signal,
+        })
+            .then(res => handleErrors(res, strings.searchProperty.errorToast.searchFailed))
+            .then(res => res.json())
+            .then((res) => {
+                if (!res.features.length) {
+                    toast.error(strings.searchProperty.errorToast.searchCoordsNoResults);
+                }
+                return res;
+            });
     }
 
-    return fetch(`api/property/${queryParameter}`, config())
+    return fetch(`api/property/${queryParameter}`, { ...config(), signal })
         .then(res => handleErrors(res, strings.searchProperty.errorToast.searchFailed))
         .then(res => res.json())
         .then((res) => {
@@ -57,5 +72,4 @@ export const fetchPropertyPdfLinks = (propertyIdentifier: string, language: stri
     }`, config())
         .then(res => handleErrors(res, strings.searchProperty.errorToast.searchLinksFailed))
         .then(res => res.json())
-        .catch(err => console.log(err))
 );
