6ef3d84c0fbb14d1706952642debae96db01692c
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2018-12-12 18:16:55 +0200

    KSR-282 Change contract list icon styles

--- src/main/client/src/components/ui/blocks/Contract/Icon.js
+++ src/main/client/src/components/ui/blocks/Contract/Icon.js
@@ -1,13 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import * as styles from '../../defaultStyles';
 
 const Icon = styled.i`
     width: 30px;
+    margin-right: ${props => props.edit && '1rem'};
         
     &:hover {
         color: ${styles.colorBackgroundDarkBlue};
         cursor: pointer;
-    }
+    };
+    
+    ${({ unlink }) => unlink && css`
+        margin-left: 1rem;
+        
+        &:hover {
+            color: ${styles.colorDanger};
+        };
+    `};
+    
+    ${({ disabled }) => disabled && css`
+        color: ${styles.colorFontDisabled};
+        
+        &:hover {
+            color: ${styles.colorFontDisabled};
+            cursor: default;
+        };
+    `};
 `;
 
 export default Icon;
