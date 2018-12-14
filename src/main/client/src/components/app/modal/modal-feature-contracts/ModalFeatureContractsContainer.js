f90c0be6c1dad12d5a80aa63cf374005b84ee15e
Markus Åfelt <markus.afelt@sitowise.com>
2018-12-14 15:19:40 +0200

    KSR-252 Add saving functionality for contracts
    Edit and Add contract data will be saved.

--- src/main/client/src/components/app/modal/modal-feature-contracts/ModalFeatureContractsContainer.js
+++ src/main/client/src/components/app/modal/modal-feature-contracts/ModalFeatureContractsContainer.js
@@ -2,15 +2,33 @@
 import { connect } from 'react-redux';
 import { removeContractListInfo } from '../../../../reducers/contract/actions';
 import ModalFeatureContracts from './ModalFeatureContracts';
+import { getContractLayers } from '../../../../utils/contracts/contracts';
+import { nestedVal } from '../../../../utils/nestedValue';
 
 const mapStateToProps = (state) => {
     const { layerId } = state.contract.contractList;
-    const currentLayer = layerId && state.map.layerGroups.layerList
-        .find(layer => layer.id === layerId.replace('.s', ''));
+
+    const {
+        currentLayer, contractLayer,
+    } = getContractLayers(layerId, state.map.layerGroups.layerList);
+
+    const createLayerPermission = state.adminTool.active.layerId === layerId &&
+        nestedVal(currentLayer, ['layerPermission', 'updateLayer']) &&
+        nestedVal(contractLayer, ['layerPermission', 'createLayer']);
+
+    const editLayerPermission = state.adminTool.active.layerId === layerId &&
+        nestedVal(currentLayer, ['layerPermission', 'updateLayer']) &&
+        nestedVal(contractLayer, ['layerPermission', 'updateLayer']);
 
     return {
         objectId: state.contract.contractList.objectId,
         currentLayer,
+        contractLayer,
+        addressField: currentLayer && currentLayer.addressField,
+        featureType: currentLayer && currentLayer.featureType,
+        view: state.map.mapView.view,
+        createLayerPermission,
+        editLayerPermission,
     };
 };
 
