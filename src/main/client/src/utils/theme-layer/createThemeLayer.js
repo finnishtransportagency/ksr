fa2025612082c12523db6982a61fb753a8c1fb55
Riku Rohkea <riku.rohkea@sitowise.com>
2019-01-02 14:13:59 +0200

    KSR-94 Create util for creating theme layer

---
+++ src/main/client/src/utils/theme-layer/createThemeLayer.js
@@ -0,0 +1,38 @@
+// @flow
+
+import esriLoader from 'esri-loader';
+
+/**
+ * Create theme layer for given feature layer by setting new ClassBreaksRenderer with given
+ * parameters.
+ *
+ * @param {Object} featureLayer Layer which the theme layer is created for.
+ * @param {Object} rendererParams Object containing parameters for creating new ClassBreaksRenderer.
+ * @param {string} layerId Layer identifier.
+ * @param {Object[]} layerList List of layers.
+ * @param {Function} setLayerList Function for setting updated layer list.
+ *
+ * @returns {Promise} Returns when new renderer has been created.
+ */
+export const createThemeLayer = async (
+    featureLayer: Object,
+    rendererParams: Object,
+    layerId: string,
+    layerList: Object[],
+    setLayerList: (Object[]) => void,
+) => {
+    const [colorRendererCreator] = await esriLoader
+        .loadModules(['esri/renderers/smartMapping/creators/color']);
+    const response = await colorRendererCreator.createClassBreaksRenderer(rendererParams);
+    const newLayerList = layerList.map(layer => ({
+        ...layer,
+        renderer: layer.id === layerId && !layer.renderer
+            ? featureLayer.renderer
+            : layer.renderer,
+    }));
+
+    featureLayer.renderer = response.renderer;
+    featureLayer.legendEnabled = true;
+
+    setLayerList(newLayerList);
+};
