fc7a11b80424e80fa9daedbede43775562d1ef96
Topi Penttinen <topi.penttinen@sitowise.com>
2019-01-04 15:03:44 +0200

    KSR-324 Add new method to search referencing layers

--- src/main/java/fi/sitowise/ksr/service/LayerService.java
+++ src/main/java/fi/sitowise/ksr/service/LayerService.java
@@ -4,9 +4,12 @@ import fi.sitowise.ksr.domain.Layer;
 import fi.sitowise.ksr.domain.LayerAction;
 import fi.sitowise.ksr.repository.LayerRepository;
 import fi.sitowise.ksr.repository.UserLayerRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 import static fi.sitowise.ksr.utils.KsrAuthenticationUtils.getCurrentUserGroups;
@@ -16,6 +19,7 @@ import static fi.sitowise.ksr.utils.KsrAuthenticationUtils.getCurrentUserGroups;
  */
 @Service
 public class LayerService {
+    private static final Logger log = LogManager.getLogger(LayerService.class);
 
     private final LayerRepository layerRepository;
     private final UserLayerRepository userLayerRepository;
@@ -48,5 +52,20 @@ public class LayerService {
 
         //TODO: Check permission for userlayer
         return layer == null ? userLayerRepository.getUserLayer(id) : layer;
+    }
+
+    /**
+     * Gets layers that reference to given layer.
+     *
+     * @param layerId layer's id
+     * @return list of layers
+     */
+    List<Layer> getReferencingLayers(String layerId) {
+        List<String> userGroups = getCurrentUserGroups();
+        if (userGroups == null) {
+            log.info("Failed to query referencing layers. User has no userGroups.");
+            return Collections.emptyList();
+        }
+        return layerRepository.getReferencingLayers(Integer.parseInt(layerId), userGroups);
     }
 }
