03583300798af5fd44e3503ffc5cdfecf97eb775
Markus Åfelt <markus.afelt@sitowise.com>
2019-01-08 09:05:11 +0200

    KSR-176 Add layerGroup name to active layer view

--- src/main/client/src/components/app/shared/styles.js
+++ src/main/client/src/components/app/shared/styles.js
@@ -1,12 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const MapLayerTitleWrapper = styled.div`
+    display: flex;
+    
+    ${props => props.showLayerGroup && css`
+        font-size: 14px;
+        padding: 0 0 0.2em 0;
+    `};  
+    
+    ${props => props.color && css`
+            color: ${props.color};
+    `};
+`;
+
+export const Icon = styled.div`
+    font-size: 0.8rem;
+    min-width: 1rem;
+    max-width: 1rem;
+`;
+
+export const Text = styled.div`
     overflow: hidden;
     -ms-text-overflow: ellipsis;
     text-overflow: ellipsis;
-    
-    i {
-        padding-right: 0.5em;
-        font-size: 0.8em;
-    }
 `;
