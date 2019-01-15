25c7f36ffcc4d9ef6a328c7b225b42f7108f8da7
Topi Penttinen <topi.penttinen@sitowise.com>
2019-01-15 15:25:43 +0200

    KSR-332 Add usefull methods for Layer
    - Add methods to construct different urls for layers

--- src/main/java/fi/sitowise/ksr/domain/Layer.java
+++ src/main/java/fi/sitowise/ksr/domain/Layer.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
