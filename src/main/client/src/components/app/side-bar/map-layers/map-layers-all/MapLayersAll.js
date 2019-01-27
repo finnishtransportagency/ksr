6b2ce027362d152eca146588554a914a7cefbf25
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-01-27 19:08:08 +0200

    KSR-314 Improve layer activation to use new redux actions

--- src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAll.js
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAll.js
@@ -2,21 +2,15 @@
 import React, { Component } from 'react';
 import LoadingIcon from '../../../shared/LoadingIcon';
 import MapLayersAllView from './MapLayersAllView';
-import { reorderLayers } from '../../../../../utils/reorder';
-import { getLayerFields } from '../../../../../utils/map';
 
 type Props = {
     layerGroups: Array<any>,
     layerList: any,
     subLayers: Object[],
     fetching: boolean,
-    setLayerList: (Array<any>) => void,
-    activeAdminTool: string,
-    view: Object,
-    setActiveAdminTool: (layerId: string, layerList: Array<any>) => void,
-    addNonSpatialContentToTable: Object => void,
-    setLoading: Function,
-    removeLoading: Function,
+    loadingLayers: string[],
+    activateLayers: (layers: Object[]) => void,
+    deactivateLayer: (layerId: string) => void,
 };
 
 type State = {
@@ -62,114 +56,48 @@ class MapLayersActive extends Component<Props, State> {
         }
     };
 
-    handleLayerClick = async (id: number) => {
-        const {
-            setLayerList,
-            activeAdminTool,
-            setActiveAdminTool,
-            layerGroups,
-            view,
-            addNonSpatialContentToTable,
-            setLoading,
-            removeLoading,
-        } = this.props;
-
-        let layerList = [...this.props.layerList];
+    handleLayerClick = (id: number) => {
+        const { activateLayers, deactivateLayer, layerList } = this.props;
         const foundLayer = layerList.find(l => l.id === id);
-        view.popup.close();
-
-        if (foundLayer.id === activeAdminTool) setActiveAdminTool('', layerList);
-
-        layerList = layerList.map(layer => ({
-            ...layer,
-            active: layer.id === foundLayer.id ? !layer.active : layer.active,
-            visible: layer.id === foundLayer.id ? !layer.visible : layer.visible,
-        }));
-        layerList = reorderLayers(layerGroups, layerList, foundLayer);
 
         if (!foundLayer.active) {
-            if (foundLayer.type === 'agfl') {
-                setLoading();
-                layerList = await getLayerFields(layerList, [foundLayer]);
-                removeLoading();
-                setLayerList(layerList);
-                addNonSpatialContentToTable(foundLayer);
-            } else {
-                setLoading(); // Loading should be removed after layer has been added to map.
-                layerList = await getLayerFields(layerList, [foundLayer]);
-                setLayerList(layerList);
-            }
+            activateLayers([foundLayer]);
         } else {
-            setLayerList(layerList);
+            deactivateLayer(foundLayer.id);
         }
     };
 
-    handleLayerGroupClick = async (layerGroupName: string) => {
-        const {
-            setLayerList,
-            layerGroups,
-            addNonSpatialContentToTable,
-        } = this.props;
-        const layerList = [...this.props.layerList];
+    handleLayerGroupClick = (layerGroupName: string) => {
+        const { layerList } = this.props;
         const foundLayers = layerList.filter(l =>
-            l.layerGroupName === layerGroupName &&
-            l.relationType !== 'link');
+            l.layerGroupName === layerGroupName
+            && l.relationType !== 'link'
+            && l._source !== 'shapefile');
 
-        await this.updateLayerList(
-            foundLayers,
-            [...this.props.layerList],
-            layerGroups,
-            addNonSpatialContentToTable,
-            setLayerList,
-        );
+        this.updateLayerList(foundLayers);
     };
 
-    handleSubLayerGroupClick = async (id: number) => {
-        const {
-            setLayerList,
-            layerGroups,
-            subLayers,
-            addNonSpatialContentToTable,
-        } = this.props;
-        const layerList = [...this.props.layerList];
+    handleSubLayerGroupClick = (id: number) => {
+        const { subLayers, layerList } = this.props;
         const parentLayer = layerList.filter(l => l.id === id);
         const filteredSublayers = subLayers.filter(l => l.parentLayer === id);
         const foundLayers = [...new Set([...parentLayer, ...filteredSublayers])];
 
-        await this.updateLayerList(
-            foundLayers,
-            layerList,
-            layerGroups,
-            addNonSpatialContentToTable,
-            setLayerList,
-        );
+        this.updateLayerList(foundLayers);
     };
 
-    updateLayerList = async (
-        foundLayers: Object[],
-        layerList: Object[],
-        layerGroups: Object[],
-        addNonSpatialContentToTable: Function,
-        setLayerList: Function,
-    ) => {
+    updateLayerList = (foundLayers: Object[]) => {
+        const { activateLayers, deactivateLayer, loadingLayers } = this.props;
         const active = foundLayers.every(l => l.active);
-        let modifiedLayerList = layerList.map(layer => ({
-            ...layer,
-            active: foundLayers.find(f => f.id === layer.id) ? !active : layer.active,
-            visible: foundLayers.find(f => f.id === layer.id) ? !active : layer.visible,
-        }));
 
-        modifiedLayerList = await getLayerFields(modifiedLayerList, foundLayers);
-
-        await foundLayers.forEach(async (l) => {
-            if (!l.active) {
-                modifiedLayerList = reorderLayers(layerGroups, modifiedLayerList, l);
-                if (l.type === 'agfl') {
-                    addNonSpatialContentToTable(l);
-                }
-            }
-        });
-        setLayerList(modifiedLayerList);
+        if (active) {
+            foundLayers.forEach(foundLayer => deactivateLayer(foundLayer.id));
+        } else {
+            const layersToBeActivated = foundLayers.filter(foundLayer => (
+                !foundLayer.active && !loadingLayers.some(layerId => layerId === foundLayer.id)
+            ));
+            if (layersToBeActivated.length) activateLayers(layersToBeActivated);
+        }
     };
 
     render() {
--- src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAllContainer.js
+++ src/main/client/src/components/app/side-bar/map-layers/map-layers-all/MapLayersAllContainer.js
@@ -1,7 +1,6 @@
 // @flow
 import { connect } from 'react-redux';
-import { setLayerList } from '../../../../../reducers/map/actions';
-import { setLoading, removeLoading } from '../../../../../reducers/loading/actions';
+import { setLayerList, activateLayers, deactivateLayer } from '../../../../../reducers/map/actions';
 import { setActiveAdminTool } from '../../../../../reducers/adminTool/actions';
 import { addNonSpatialContentToTable } from '../../../../../reducers/table/actions';
 import MapLayersAll from './MapLayersAll';
@@ -13,6 +12,7 @@ const mapStateToProps = state => ({
     fetching: state.map.layerGroups.fetching,
     activeAdminTool: state.adminTool.active.layerId,
     view: state.map.mapView.view,
+    loadingLayers: state.loading.loadingLayers,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -25,11 +25,11 @@ const mapDispatchToProps = dispatch => ({
     addNonSpatialContentToTable: (layer) => {
         dispatch(addNonSpatialContentToTable(layer));
     },
-    setLoading: () => {
-        dispatch(setLoading());
+    activateLayers: (layers) => {
+        dispatch(activateLayers(layers));
     },
-    removeLoading: () => {
-        dispatch(removeLoading());
+    deactivateLayer: (layerId) => {
+        dispatch(deactivateLayer(layerId));
     },
 });
 
