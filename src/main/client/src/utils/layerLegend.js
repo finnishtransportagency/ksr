16a5ba1990d3909d2edde1e75bdca8d1bce55f18
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-01-27 23:39:38 +0200

    KSR-314 Changed layer legend method to take single layer as parameter

--- src/main/client/src/utils/layerLegend.js
+++ src/main/client/src/utils/layerLegend.js
@@ -4,38 +4,35 @@ import esriLoader from 'esri-loader';
 /**
  * Render layer legend previews for given layers.
  *
- * @param {Object[]} layerList List of layers.
+ * @param {Object} layer List of layers.
  * @param {Object} view Esri map view.
  *
- * @returns {Promise} Promise object that contains updated layerList with legend symbols.
+ * @returns {Promise} Promise object that contains updated layer with legend symbol.
  */
 export const getLayerLegend = async (
-    layerList: Object[],
+    layer: Object,
     view: Object,
 ) => {
     const [symbolPreview] = await esriLoader
         .loadModules(['esri/symbols/support/symbolPreview']);
 
-    return Promise.all(layerList.map(l => ({ ...l }))
-        .map(async (l) => {
-            if (l.type === 'agfs' && l.active && !l.legendSymbol) {
-                const fl = view.map.findLayerById(l.id);
-                if (fl && fl.renderer) {
-                    const setLegendSymbol = async (symbol: Object) => {
-                        l.legendSymbol = await symbolPreview.renderPreviewHTML(symbol, {
-                            size: 12,
-                        });
-                    };
-                    if (fl.renderer.symbol) {
-                        const symbol = fl.renderer.symbol.clone();
-                        await setLegendSymbol(symbol);
-                    } else if (fl.renderer.uniqueValueInfos) {
-                        // Select first symbol if layer has multiple legends.
-                        const symbol = fl.renderer.uniqueValueInfos[0].symbol.clone();
-                        await setLegendSymbol(symbol);
-                    }
-                }
+    if (layer.type === 'agfs' && !layer.legendSymbol) {
+        const fl = view.map.findLayerById(layer.id);
+        if (fl && fl.renderer) {
+            const setLegendSymbol = async (symbol: Object) => {
+                layer.legendSymbol = await symbolPreview.renderPreviewHTML(symbol, {
+                    size: 12,
+                });
+            };
+            if (fl.renderer.symbol) {
+                const symbol = fl.renderer.symbol.clone();
+                await setLegendSymbol(symbol);
+            } else if (fl.renderer.uniqueValueInfos) {
+                // Select first symbol if layer has multiple legends.
+                const symbol = fl.renderer.uniqueValueInfos[0].symbol.clone();
+                await setLegendSymbol(symbol);
             }
-            return l;
-        }));
+        }
+    }
+    return layer;
 };
