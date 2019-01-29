b2e6c09c6c9eb867f2a047d3bfa7dc2341596f83
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-01-29 17:04:31 +0200

    KSR-314 Remove admin check from layer search

--- src/main/client/src/components/app/side-bar/search/search-layer/SearchLayerContainer.js
+++ src/main/client/src/components/app/side-bar/search/search-layer/SearchLayerContainer.js
@@ -6,17 +6,10 @@ import SearchLayer from './SearchLayer';
 import strings from '../../../../../translations';
 
 const mapStateToProps = (state) => {
-    let allQueryableLayers = [];
+    const allQueryableLayers = state.map.layerGroups.layerList
+        .filter(l => l.queryable && l._source !== 'search')
+        .map(l => ({ ...l, value: l.id, label: l.name }));
 
-    if (state.adminTool.active.layerId) {
-        allQueryableLayers = state.map.layerGroups.layerList
-            .filter(l => l.queryable && l.id === state.adminTool.active.layerId && l._source !== 'search')
-            .map(l => ({ ...l, value: l.id, label: l.name }));
-    } else {
-        allQueryableLayers = state.map.layerGroups.layerList
-            .filter(l => l.queryable && l._source !== 'search')
-            .map(l => ({ ...l, value: l.id, label: l.name }));
-    }
     const activeQueryableLayers = allQueryableLayers.filter(l => l.active);
     const queryOptions = [{ value: 'queryAll', label: strings.search.allQueryableLayers }]
         .concat([{ value: 'queryActive', label: strings.search.allActiveLayers }])
