ee05720c2422053ebcd6ae29cde4007798295f7f
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-02-07 17:18:18 +0200

    KSR-358 Add api calls for linking and unlinking contract

--- src/main/client/src/api/contract/contractRelations.js
+++ src/main/client/src/api/contract/contractRelations.js
@@ -1,5 +1,5 @@
 // @flow
-import { config, handleErrors } from '../config';
+import { config, getHeaders, handleErrors } from '../config';
 
 /**
  * Gets contract relation data.
@@ -13,5 +13,61 @@ export const fetchContractRelation = (layerId: number, objectId: number): any =>
     fetch(`api/contract/${layerId}/${objectId}`, config())
         .then(handleErrors)
         .then(r => r.json())
+        .catch(err => console.log(err))
+);
+
+/**
+ * Link contract to layer feature.
+ *
+ * @param {number} layerId Id of the layer whose object to relate.
+ * @param {number} layerObjectId Id of the object to relate.
+ * @param {number} contractLayerId Id of the contract layer in which to relate.
+ * @param {number} contractObjectId Id of the object in which to relate.
+ *
+ * @returns {Promise<string | null>} Returns 'created' or 'exists' on successful linking depending
+ * on whether the link already exists or null if linking is unsuccessful.
+ */
+export const linkContract = (
+    layerId: number,
+    layerObjectId: number,
+    contractLayerId: number,
+    contractObjectId: number,
+) => (
+    fetch(`api/contract/link/${layerId}/${layerObjectId}/${contractLayerId}/${contractObjectId}`, {
+        headers: getHeaders(),
+        method: 'POST',
+    })
+        .then((res) => {
+            if (res.ok && res.status === 201) {
+                return 'created';
+            } else if (res.ok && res.status === 200) {
+                return 'exists';
+            }
+            return null;
+        })
+        .catch(err => console.log(err))
+);
+
+/**
+ * Unlink contract from layer feature.
+ *
+ * @param {number} layerId Id of the layer whose object to unrelate.
+ * @param {number} layerObjectId Id of the object to unrelate.
+ * @param {number} contractLayerId Id of the contract layer in which to unrelate.
+ * @param {number} contractObjectId Id of the object in which to unrelate.
+ *
+ * @returns {Promise<boolean>} Returns true or false depending if unlinking was successful or not.
+ */
+export const unlinkContract = (
+    layerId: number,
+    layerObjectId: number,
+    contractLayerId: number,
+    contractObjectId: number,
+) => (
+    fetch(`api/contract/unlink/${layerId}/${layerObjectId}/${contractLayerId}/${contractObjectId}`, {
+        headers: getHeaders(),
+        method: 'POST',
+    })
+        .then(res => res.ok && res.status === 200)
         .catch(err => console.log(err))
 );
