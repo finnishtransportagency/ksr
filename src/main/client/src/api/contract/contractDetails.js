2fe08b2964f335b40d78d0778e851ce1eb912f88
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-02-13 14:24:08 +0200

    KSR-342 Add api call for getting related features

---
+++ src/main/client/src/api/contract/contractDetails.js
@@ -0,0 +1,20 @@
+// @flow
+import { config, handleErrors } from '../config';
+
+/**
+ * Gets all features that relate to given feature on given layer.
+ *
+ * @param {number} layerId Id of the layer.
+ * @param {number} layerObjectId Id of the object, whose contracts to query.
+ *
+ * @returns {Promise<Object[] | any>} List containing related layers with features.
+ */
+export const fetchContractDetails = (
+    layerId: number,
+    layerObjectId: number,
+): Promise<Object[] | any> => (
+    fetch(`api/contract/details/${layerId}/${layerObjectId}`, config())
+        .then(handleErrors)
+        .then(r => r.json())
+        .catch(err => console.log(err))
+);
