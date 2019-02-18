d192c37518aaa0b1542bfbc7119444521b31ecec
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-02-18 16:52:15 +0200

    KSR-342 Add UI block for attribute details

---
+++ src/main/client/src/components/ui/blocks/ContractFeatureAttribute/Name.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Name = styled.div`
+    font-weight: 500;
+    margin-bottom: 0.25rem;
+`;
+
+export default Name;
---
+++ src/main/client/src/components/ui/blocks/ContractFeatureAttribute/Value.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import * as styles from '../../defaultStyles';
+
+const Value = styled.div`
+    font-weight: 400;
+    color: ${styles.colorFontDisabled};
+`;
+
+export default Value;
