b62edfd8be3b2b25649525ef638dc123fdc74215
Topi Penttinen <topi.penttinen@sitowise.com>
2019-02-19 13:15:37 +0200

    KSR-369 Rewrite to work with updated react-dropzone

--- src/main/client/src/components/app/modal/modal-shapefile/__tests__/ModalShapefileView.test.js
+++ src/main/client/src/components/app/modal/modal-shapefile/__tests__/ModalShapefileView.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Dropzone from 'react-dropzone';
 import ModalShapefileView from '../ModalShapefileView';
 import { DropzoneText } from '../styles';
-import { Button } from '../../../../ui/elements/index';
+import { Button } from '../../../../ui/elements';
+import strings from '../../../../../translations';
 
 
 const setup = () => {
     const props = {
         onDrop: jest.fn(),
         acceptedExtensions: '.shp,.dbf',
+        fileUploadRef: jest.fn(),
+        closeModal: jest.fn(),
     };
-    const wrapper = shallow(<ModalShapefileView {...props} />);
+    const wrapper = mount(<ModalShapefileView {...props} />);
 
     return { props, wrapper };
 };
@@ -22,10 +25,9 @@ describe('<ModalShapefileView />', () => {
     it('should render self', () => {
         expect(wrapper.find(Dropzone).exists()).toBe(true);
     });
-    it('should render self', () => {
-        expect(wrapper.find(DropzoneText).exists()).toBe(true);
-    });
-    it('should render self', () => {
-        expect(wrapper.find(Button).exists()).toBe(true);
+    it('should render children', () => {
+        expect(wrapper.find(Button).length).toBe(1);
+        expect(wrapper.find('p').first().text()).toBe(strings.dropzoneShape.dropText);
+        expect(wrapper.find('p').last().text()).toBe(strings.dropzoneShape.orText);
     });
 });
