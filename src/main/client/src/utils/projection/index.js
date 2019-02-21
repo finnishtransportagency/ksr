13cf918610bcee9d13a8e44a4e9b138709bb0ea3
Topi Penttinen <topi.penttinen@sitowise.com>
2019-02-21 13:49:24 +0200

    KSR-318 Create a utility package for coordinate conversion

---
+++ src/main/client/src/utils/projection/index.js
@@ -0,0 +1,28 @@
+// @flow
+import proj4 from 'proj4';
+
+proj4.defs([
+    ['EPSG:3067', '+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'],
+]);
+
+/**
+ * Projects array of coordinates from sSrs to tSrs.
+ *
+ * Coordinates can be a flat array or nested array. The returned array has same
+ * shape than input array.
+ *
+ * @param {string} sSrs EPSG code for source Spatial Reference System.
+ * @param {string} tSrs EPSG code for target Spatial Reference System.
+ * @param {any[]} coords Array of coordinates.
+ * @returns {any[]} Array of projected coordinates.
+ */
+export const project = (
+    sSrs: string,
+    tSrs: string,
+    coords: any[],
+) => {
+    if (Array.isArray(coords) && coords.length && coords.every(Number.isFinite)) {
+        return proj4(sSrs, tSrs, coords);
+    }
+    return coords.reduce((acc, cur) => [...acc, project(sSrs, tSrs, cur)], []);
+};
