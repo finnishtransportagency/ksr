7c291ced6b65993e3aabdf2216450870b8e1ac5b
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-02-25 16:56:55 +0200

    KSR-342 Add flow-typed stub files
    - Fixes 'module not found' flow errors when node-modules dir ignored in flow

---
+++ src/main/client/flow-typed/npm/react-custom-scrollbars_v4.2.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: 895ec743914bd13da4f3e9f7f1851374
+// flow-typed version: c89f82535b/react-custom-scrollbars_v4.2.x/flow_>=v0.54.x
+
+declare module 'react-custom-scrollbars' {
+  declare module.exports: any;
+}
