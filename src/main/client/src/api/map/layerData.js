06f7ca019bce4bf6e9970ccf9839e15365026cb9
Riku Rohkea <riku.rohkea@sitowise.com>
2019-03-04 10:07:19 +0200

    KSR-379 Format layerData
    - Add missing flow declaration and typing.
    - Correct jsdocs.
    - Fix eslint warnings.

--- src/main/client/src/api/map/layerData.js
+++ src/main/client/src/api/map/layerData.js
@@ -1,18 +1,22 @@
+// @flow
+
 import querystring from 'querystring';
 import { config } from '../config';
 
 /**
- * Finds layer
+ * Fetch layer details from database.
  *
- * @param layerId Layer id (ID in database) that is used in fetch URL
+ * @param {string} layerId Layer id (ID in database) that is used in fetch URL. In case of search
+ * layers base layers data will be fetched.
  *
- * @returns All data found from layer, that will be passed to layerList
+ * @returns {Promise} All data found from layer that will be passed to layer list.
  */
-export const layerData = layerId =>
+export const layerData = (layerId: string) => (
     fetch(`api/proxy/layer/${layerId.replace('.s', '')}?${
         querystring.stringify({
             f: 'pjson',
         })
     }`, config())
         .then(r => r.json())
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
+);
