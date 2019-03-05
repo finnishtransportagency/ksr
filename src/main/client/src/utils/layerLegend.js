71bd4df1aa47d4f98f989f46ca2f0843fff6c049
Markus Åfelt <markus.afelt@sitowise.com>
2019-03-05 09:46:12 +0200

    KSR-388 Split getLegendSymbol to own method

--- src/main/client/src/utils/layerLegend.js
+++ src/main/client/src/utils/layerLegend.js
@@ -2,6 +2,20 @@
 import esriLoader from 'esri-loader';
 
 /**
+ * Get legend symbol.
+ *
+ * @param {Object} symbol Layer symbol.
+ * @returns {Promise} Promise html element that contains legend symbol.
+ */
+export const getLegendSymbol = async (symbol: Object) => {
+    const [symbolPreview] = await esriLoader
+        .loadModules(['esri/symbols/support/symbolPreview']);
+    return symbolPreview.renderPreviewHTML(symbol, {
+        size: 12,
+    });
+};
+
+/**
  * Render layer legend previews for given layers.
  *
  * @param {Object} layer List of layers.
@@ -13,24 +27,16 @@ export const getLayerLegend = async (
     layer: Object,
     view: Object,
 ) => {
-    const [symbolPreview] = await esriLoader
-        .loadModules(['esri/symbols/support/symbolPreview']);
-
     if (layer.type === 'agfs' && !layer.legendSymbol) {
         const fl = view.map.findLayerById(layer.id);
         if (fl && fl.renderer) {
-            const setLegendSymbol = async (symbol: Object) => {
-                layer.legendSymbol = await symbolPreview.renderPreviewHTML(symbol, {
-                    size: 12,
-                });
-            };
             if (fl.renderer.symbol) {
                 const symbol = fl.renderer.symbol.clone();
-                await setLegendSymbol(symbol);
+                layer.legendSymbol = await getLegendSymbol(symbol);
             } else if (fl.renderer.uniqueValueInfos) {
                 // Select first symbol if layer has multiple legends.
                 const symbol = fl.renderer.uniqueValueInfos[0].symbol.clone();
-                await setLegendSymbol(symbol);
+                layer.legendSymbol = await getLegendSymbol(symbol);
             }
         }
     }
