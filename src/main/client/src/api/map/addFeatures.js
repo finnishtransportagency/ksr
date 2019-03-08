51cfd6499a288aab412984f2b612d345f8ade015
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-03-08 16:49:11 +0200

    KSR-88 Improve fetches to use config method

--- src/main/client/src/api/map/addFeatures.js
+++ src/main/client/src/api/map/addFeatures.js
@@ -1,20 +1,19 @@
 // @flow
+import { config } from '../config';
 
 /**
-* Calls arcGIS Server's addFeatures endpoint.
-*
-* @param {string} layerId Id of the corresponding layer.
-* @param {string} params Params to be sent in the request.
-*
-* @return Promise Returns Promise with json body.
+ * Calls arcGIS Server's addFeatures endpoint.
+ *
+ * @param {string} layerId Id of the corresponding layer.
+ * @param {string} params Params to be sent in the request.
+ *
+ * @returns {Promise<Object>} Returns Promise with json body.
 */
-export const addFeatures = (layerId: string, params: string) =>
+export const addFeatures = (layerId: string, params: string) => (
     fetch(`api/proxy/layer/${layerId}/addFeatures`, {
-        credentials: 'include',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        },
+        ...config(),
         method: 'POST',
         body: params,
     })
-        .then(r => r.json());
+        .then(r => r.json())
+);
