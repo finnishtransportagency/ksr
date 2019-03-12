4afdacc083487050a35b92fb52269274a64bc3b9
Topi Penttinen <topi.penttinen@sitowise.com>
2019-03-12 12:48:53 +0200

    KSR-366 Add missing but necessary OAM-headers

--- src/main/client/src/setupProxy.js
+++ src/main/client/src/setupProxy.js
@@ -1,12 +1,14 @@
 const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.use(proxy('/api', {
-        'target': 'http://localhost:8080',
-        'testURL': 'http://localhost/',
-        'headers': {
-          'OAM_REMOTE_USER': 'dev_user',
-          'OAM_GROUPS': 'KSR_ROLE_ADMIN',
-        }
-    }))
-}
+        target: 'http://localhost:8080',
+        testURL: 'http://localhost/',
+        headers: {
+            OAM_REMOTE_USER: 'dev_user',
+            OAM_GROUPS: 'KSR_ROLE_ADMIN',
+            OAM_USER_FIRST_NAME: 'Test',
+            OAM_USER_LAST_NAME: 'User',
+        },
+    }));
+};
