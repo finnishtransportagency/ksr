0d5583d0d8bd7ad0bdec3b06b422ccb7033cb089
Jere Pallaskorpi <jere.pallaskorpi@sitowise.com>
2019-04-09 14:02:09 +0300

    KSR-404 Add contract document controller

---
+++ src/main/java/fi/sitowise/ksr/controller/ContractDocumentController.java
@@ -0,0 +1,46 @@
+package fi.sitowise.ksr.controller;
+
+import fi.sitowise.ksr.exceptions.KsrApiException;
+import fi.sitowise.ksr.service.ContractDocumentService;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Controller for contract documents.
+ */
+@RestController
+@RequestMapping("/api/contract-document")
+public class ContractDocumentController {
+    private final ContractDocumentService contractDocumentService;
+
+    @Autowired
+    private ContractDocumentController(ContractDocumentService contractDocumentService) {
+        this.contractDocumentService = contractDocumentService;
+    }
+
+    /**
+     * Handles redirecting to alfresco or case management search URL.
+     *
+     * @param request HTTP Request. Contains document type and search value as query parameters.
+     * @return Redirects to contract document URL.
+     */
+    @ApiOperation("Redirects to contract document URL")
+    @GetMapping(value = "")
+    public RedirectView redirectToContractDocumentUrl(HttpServletRequest request) {
+        String documentType = request.getParameter("documentType");
+        String searchValue = request.getParameter("searchValue");
+
+        if (StringUtils.isEmpty(documentType) || StringUtils.isEmpty(searchValue)) {
+            throw new KsrApiException.BadRequestException("Invalid query parameters given.");
+        }
+
+        return new RedirectView(contractDocumentService.getContractDocumentUrl(documentType, searchValue));
+    }
+}
---
+++ src/test/java/fi/sitowise/ksr/controller/ContractDocumentControllerTests.java
@@ -0,0 +1,103 @@
+package fi.sitowise.ksr.controller;
+
+import fi.sitowise.ksr.exceptions.KsrApiException;
+import fi.sitowise.ksr.helper.AuthControllerTestBase;
+import fi.sitowise.ksr.service.ContractDocumentService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Contract document controller tests.
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {ContractDocumentController.class})
+public class ContractDocumentControllerTests extends AuthControllerTestBase {
+
+    @MockBean
+    ContractDocumentService contractDocumentService;
+
+    @Before
+    public void setup() {
+        init();
+    }
+
+    private String createTestUrl(InvocationOnMock invocation) {
+        Object[] args = invocation.getArguments();
+        String testUrl = "http://test.url.com";
+        String testType = (String) args[0];
+        String testValue = (String) args[1];
+
+        return String.format("%s?documentType=%s&searchValue=%s", testUrl, testType, testValue);
+    }
+
+    @Test
+    public void testRedirectToContractDocumentUrlAlfrescoIsFoundAndRedirected() throws Exception {
+        Mockito.when(contractDocumentService.getContractDocumentUrl(Mockito.eq("alfresco"), Mockito.anyString()))
+                .thenAnswer((Answer<String>) this::createTestUrl);
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "alfresco")
+                .param("searchValue", "123"))
+                .andExpect(status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://test.url.com?documentType=alfresco&searchValue=123"));
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "alfresco")
+                .param("searchValue", "789"))
+                .andExpect(status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://test.url.com?documentType=alfresco&searchValue=789"));
+    }
+
+    @Test
+    public void testRedirectToContractDocumentUrlCaseManagementIsFoundAndRedirected() throws Exception {
+        Mockito.when(contractDocumentService.getContractDocumentUrl(Mockito.eq("caseManagement"), Mockito.anyString()))
+                .thenAnswer((Answer<String>) this::createTestUrl);
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "caseManagement")
+                .param("searchValue", "123"))
+                .andExpect(status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://test.url.com?documentType=caseManagement&searchValue=123"));
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "caseManagement")
+                .param("searchValue", "789"))
+                .andExpect(status().isFound())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("http://test.url.com?documentType=caseManagement&searchValue=789"));
+    }
+
+    @Test(expected = KsrApiException.BadRequestException.class)
+    public void testRedirectToContractDocumentUrlIsBadRequest() throws Exception {
+        Mockito.when(contractDocumentService.getContractDocumentUrl(Mockito.anyString(), Mockito.anyString()))
+                .thenThrow(KsrApiException.BadRequestException.class);
+
+        Mockito.when(contractDocumentService.getContractDocumentUrl(Mockito.anyString(), Mockito.anyString()))
+                .thenThrow(KsrApiException.BadRequestException.class);
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "")
+                .param("searchValue", ""));
+
+        this.mockMvc.perform(get("/api/contract-document")
+                .headers(this.getHeadersWithGroup("KSR_ROLE_ADMIN"))
+                .param("documentType", "asd")
+                .param("searchValue", "123"));
+    }
+}
