8db8773b663f273bab573dd0eef5284683b68887
Jani Ylikorpi <Jani.Ylikorpi@sitowise.com>
2020-01-10 13:41:16 +0200

    KRS-436 basically rewrite the whole new feature

--- src/main/client/src/components/app/modal/modal-contract-details/ModalContractDetailsContainer.js
+++ src/main/client/src/components/app/modal/modal-contract-details/ModalContractDetailsContainer.js
@@ -7,18 +7,17 @@ import { showConfirmModal } from '../../../../reducers/confirmModal/actions';
 
 const mapStateToProps = (state) => {
     const { layerId, contractObjectId, source } = state.modal.activeModal.data;
-    const { layerList, layerGroups } = state.map.layerGroups;
+    const { layerList } = state.map.layerGroups;
     const { layerId: activeAdmin } = state.adminTool.active;
     const contractLayer = layerList.find(l => l.id === layerId);
     const tableFeaturesLayers = state.table.features.layers;
-    const view = state.map.mapView.view;
+    const { view } = state.map.mapView;
 
     return {
         contractLayer,
         contractObjectId,
         source,
         layerList,
-        layerGroups,
         activeAdmin,
         tableFeaturesLayers,
         view,
