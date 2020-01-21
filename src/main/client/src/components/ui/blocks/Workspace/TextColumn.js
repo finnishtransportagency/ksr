d6535ddbff35f70ab8e49fdb5362385816edefd2
Jere Pallaskorpi <jere.pallaskorpi@gmail.com>
2020-01-21 13:45:16 +0200

    KSR-494 Add separated views for public and user workspaces

--- src/main/client/src/components/ui/blocks/Workspace/TextColumn.js
+++ src/main/client/src/components/ui/blocks/Workspace/TextColumn.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import * as styles from '../../defaultStyles';
 
 const TextColumn = styled.div`
@@ -15,13 +15,22 @@ const TextColumn = styled.div`
         }
     }
     
-    div:first-of-type {
-        font-weight: bold;
-    }
+    ${props => props.userWorkspace && css`
+        div:first-of-type {
+            font-weight: bold;
+        }
     
-    div:last-of-type {
-        font-size: 0.85em;
-    }
+        div:last-of-type {
+            font-size: 0.85em;
+        }
+    `};
+    
+    ${props => props.publicWorkspace && css`
+        div {
+            padding: 1rem;
+            font-weight: bold;
+        }
+    `};
 `;
 
 export default TextColumn;
