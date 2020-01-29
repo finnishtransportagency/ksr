9fb87fe0bc13b002358d2c69014ff4aac84bb0af
Pasi Savolainen <pasi.savolainen@sitowise.com>
2020-01-29 07:53:07 +0200

    KSR-483, revision of contract modal content

---
+++ src/main/resources/db/migration/V20200128114609423__Update_layer_contract_fields.sql
@@ -0,0 +1,3 @@
+update layer set contract_id_field = 'VAYLATYYPPI' where name = 'Maanvuokrasopimukset (kaikki)';
+update layer set contract_id_field = 'RAK_NRO',contract_description_field = 'RAK_NIMI' where name = 'Rakennukset (kaikki)';
+update layer set contract_id_field = 'COMMUNITY_NAME' where name = 'Tilat';
