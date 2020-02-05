ad01cfea7c7c8cca5f66d07ae717cfc8dc11d812
janiylikorpi <jylikorpi@hotmail.com>
2020-02-05 14:07:42 +0200

    KSR-463 Improve modal to be draggable

--- src/main/client/src/components/ui/blocks/Modal/Blur.js
+++ src/main/client/src/components/ui/blocks/Modal/Blur.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 const Blur = styled.div`
     position: fixed;
     background: rgba(0, 0, 0, 0.5);
-    top: 0;
-    left: 0;
     width: 100%;
     height: 100%;
-    opacity: 0.5; 
+    display: flex;
+    justify-content: center;
+    z-index: 999;
 `;
 
 export default Blur;
