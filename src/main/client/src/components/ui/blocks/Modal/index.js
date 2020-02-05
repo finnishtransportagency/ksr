edfe1af86ad2fb787f2b40e8dd3fee496e73f88e
janiylikorpi <jylikorpi@hotmail.com>
2020-02-05 14:07:42 +0200

    KSR-463 Improve modal to be draggable

--- src/main/client/src/components/ui/blocks/Modal/index.js
+++ src/main/client/src/components/ui/blocks/Modal/index.js
@@ -9,20 +9,16 @@ import Header from './Header';
 const Modal = styled.div`
     background: ${styles.colorBackgroundWhite};
     color: ${styles.colorFontDark};
-    position: fixed;
-    right: 0;
-    left: 0;
-    top: 2rem;
+    margin-top: 2rem;
     visibility: visible;
-    margin: 0 auto;
     max-height: 90%;
+    height: fit-content;
     width: 700px;
     z-index: 1050;
     -webkit-box-shadow: ${styles.shadowDefault};
     -moz-box-shadow: ${styles.shadowDefault};
     box-shadow: ${styles.shadowDefault};
     border: none;
-    transition: 0.6s;
     background: #FFFFFF;
     overflow: hidden;
     -webkit-animation: ${fadeInModal} 0.3s;
@@ -47,6 +43,7 @@ const Modal = styled.div`
     
     @media only screen and (max-width: 768px) {
         width: 90%;
+        left: 5%;
     };
 `;
 
