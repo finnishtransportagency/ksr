29b622ef1a173e9330ba38337bccadf9fd5dedfd
Jere Pallaskorpi <jere.pallaskorpi@gmail.com>
2020-03-16 17:07:25 +0200

    KSR-495 Add util for getting child layer domain values

--- src/main/client/src/utils/fields.js
+++ src/main/client/src/utils/fields.js
@@ -1,4 +1,7 @@
 // @flow
+import { nestedVal } from './nestedValue';
+import store from '../store';
+
 export const notAllowedFields = ['objectid', 'object', 'id', 'fid', 'symbolidentifier',
     'objectid_1', 'contract_uuid', 'shape', 'link_objectid'];
 
@@ -12,3 +15,27 @@ export const notAllowedFields = ['objectid', 'object', 'id', 'fid', 'symbolident
 export const filterNotAllowedFields = (fields: Object[]): Object[] => (
     fields.filter(f => !notAllowedFields.includes(f.name.toString().toLowerCase()))
 );
+
+/**
+ * Finds domain field values for child layers that are created from multiple different layers.
+ *
+ * @param {Object} childField Field that domain values are searched for.
+ *
+ * @returns {Object} Field with found domain values or field's domain as null.
+ */
+export const childLayerDomainValues = (childField: Object) => {
+    if (childField.domain !== null) return childField;
+
+    const { layerList } = store.getState().map.layerGroups;
+    const domainFields = layerList.filter(layer => layer.fields)
+        .flatMap(layer => layer.fields)
+        .filter(field => field.domain);
+
+    return {
+        ...childField,
+        domain: nestedVal(
+            domainFields.find(field => childField.name === field.name),
+            ['domain'],
+        ),
+    };
+};
