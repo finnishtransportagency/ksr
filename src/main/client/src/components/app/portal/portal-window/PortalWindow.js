81b3c0c4a86e00508a1d5a98fd092ca36fa64666
janiylikorpi <jylikorpi@hotmail.com>
2020-03-26 13:02:23 +0200

    pick KSR-321 Modify PortalWindow.js
    - remove document.write functions
    - use only document.createElement and document.appendChild

--- src/main/client/src/components/app/portal/portal-window/PortalWindow.js
+++ src/main/client/src/components/app/portal/portal-window/PortalWindow.js
@@ -31,12 +31,15 @@ class PortalWindow extends Component<Props, State> {
         let { externalWindow, elementContainer } = this.state;
         externalWindow = window.open('', '', `width=${window.screen.availWidth},height=${window.screen.availHeight - 58}`);
         const base: any = document.createElement('base');
+        base.href = `${location.origin}${location.pathname}`;
+        externalWindow.document.head.appendChild(base);
+        elementContainer = document.createElement('div');
+        externalWindow.document.body.appendChild(elementContainer);
+        externalWindow.document.title = strings.portalWindow.portalTitle;
+        const base: any = document.createElement('base');
         // eslint-disable-next-line no-restricted-globals
         base.href = `${location.origin}${location.pathname}`;
         externalWindow.document.head.appendChild(base);
-        externalWindow.document.title = strings.portalWindow.portalTitle;
-        elementContainer = document.createElement('div');
-        externalWindow.document.body.appendChild(elementContainer);
         this.setState({ externalWindow, elementContainer });
         externalWindow.onbeforeunload = this.handleClose;
         externalWindow.moveTo(0, 0);
@@ -57,12 +60,7 @@ class PortalWindow extends Component<Props, State> {
 
     copyStyles = (sourceDoc: any, targetDoc: any) => {
         Array.from(sourceDoc.styleSheets).forEach((styleSheet) => {
-            try {
-                if (styleSheet.cssRules) {
-                    const newStyleEl = sourceDoc.createElement('style');
-
-                    Array.from(styleSheet.cssRules).forEach((cssRule) => {
-                        newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
+            targetDoc.head.appendChild(styleSheet.ownerNode.cloneNode(true));
                     });
 
                     targetDoc.head.appendChild(newStyleEl);
@@ -76,21 +74,7 @@ class PortalWindow extends Component<Props, State> {
             } catch (e) {
                 // no need to do anything - ignore
             }
-                    });
-
-                    targetDoc.head.appendChild(newStyleEl);
-                } else {
-                    const newLinkEl = sourceDoc.createElement('link');
-
-                    newLinkEl.rel = 'stylesheet';
-                    newLinkEl.href = styleSheet.href;
-                    targetDoc.head.appendChild(newLinkEl);
-                }
-            } catch (e) {
-                // console.log(e);
-            }
         });
-        Array.from(sourceDoc.fonts).forEach(font => targetDoc.fonts.add(font));
     }
 
     render() {
