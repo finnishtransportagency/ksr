2cfc6b387a6fb5ff3ad5a42ce304bb8869a58926
janiylikorpi <jylikorpi@hotmail.com>
2020-03-26 13:02:23 +0200

    KSR-321 Modify PortalWindow.js
    - remove document.write functions
    - use only document.createElement and document.appendChild

--- src/main/client/src/components/app/portal/portal-window/PortalWindow.js
+++ src/main/client/src/components/app/portal/portal-window/PortalWindow.js
@@ -30,14 +30,14 @@ class PortalWindow extends Component<Props, State> {
     componentDidMount() {
         let { externalWindow, elementContainer } = this.state;
         externalWindow = window.open('', '', `width=${window.screen.availWidth},height=${window.screen.availHeight - 58}`);
-        // eslint-disable-next-line no-restricted-globals
-        externalWindow.document.write(`<base href="${location.origin}${location.pathname}">`);
-        externalWindow.document.write('<html><head><link href="css/all.min.css" rel="stylesheet" type="text/css"></head></html>');
-        externalWindow.document.title = strings.portalWindow.portalTitle;
         elementContainer = document.createElement('div');
         externalWindow.document.body.appendChild(elementContainer);
+        externalWindow.document.title = strings.portalWindow.portalTitle;
+        const base: any = document.createElement('base');
+        // eslint-disable-next-line no-restricted-globals
+        base.href = `${location.origin}${location.pathname}`;
+        externalWindow.document.head.appendChild(base);
         this.setState({ externalWindow, elementContainer });
-        this.copyStyles(document, externalWindow.document);
         externalWindow.onbeforeunload = this.handleClose;
         externalWindow.moveTo(0, 0);
         externalWindow.resizeTo(window.screen.width, window.screen.height);
@@ -57,27 +57,8 @@ class PortalWindow extends Component<Props, State> {
 
     copyStyles = (sourceDoc: any, targetDoc: any) => {
         Array.from(sourceDoc.styleSheets).forEach((styleSheet) => {
-            try {
-                if (styleSheet.cssRules) {
-                    const newStyleEl = sourceDoc.createElement('style');
-
-                    Array.from(styleSheet.cssRules).forEach((cssRule) => {
-                        newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText));
-                    });
-
-                    targetDoc.head.appendChild(newStyleEl);
-                } else {
-                    const newLinkEl = sourceDoc.createElement('link');
-
-                    newLinkEl.rel = 'stylesheet';
-                    newLinkEl.href = styleSheet.href;
-                    targetDoc.head.appendChild(newLinkEl);
-                }
-            } catch (e) {
-                // console.log(e);
-            }
+            targetDoc.head.appendChild(styleSheet.ownerNode.cloneNode(true));
         });
-        Array.from(sourceDoc.fonts).forEach(font => targetDoc.fonts.add(font));
     }
 
     render() {
