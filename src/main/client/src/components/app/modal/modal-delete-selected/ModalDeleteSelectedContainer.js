599504ffa62bb79cb60772c98f7fe01633d2c8c4
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2020-04-03 13:29:01 +0300

    KSR-412 Change selecteddata to get values from columns names (querycolumns or field columns)

--- src/main/client/src/components/app/modal/modal-delete-selected/ModalDeleteSelectedContainer.js
+++ src/main/client/src/components/app/modal/modal-delete-selected/ModalDeleteSelectedContainer.js
@@ -16,21 +16,24 @@ const mapStateToProps = (state) => {
     const layer = state.map.layerGroups.layerList
         .find(l => l.id === state.adminTool.active.layerId);
 
+    const columns = (queryColumnsList.length > 0) ? queryColumnsList : Object.keys(selectedData[0])
+        .filter((key, index) => key.includes('/') && index < 5);
+
     const filteredData = [];
     selectedData.map((d) => {
         const filtered = Object.keys(d)
-            .filter(key => queryColumnsList.find(qc => qc === key || key === '_id' || key.includes('/')))
+            .filter(key => columns.find(qc => key.includes(qc) || key === '_id'))
             .reduce((obj, key) => {
                 const attributeName = key.split('/').pop();
                 const layerFieldInfo = layer.fields
                     .filter(field => field.name === attributeName)[0];
+
                 const label = layerFieldInfo ? layerFieldInfo.label : attributeName;
                 const domain = layerFieldInfo && layerFieldInfo.domain ? layerFieldInfo.domain : {};
                 const value = getCodedValue(domain, d[key]);
                 obj[label] = value;
                 return obj;
             }, {});
-
         return filteredData.push(filtered);
     });
 
