8f7e86968d12bd0bb89b6f3399c4abf045a78484
mihuh <miika.huhtanen@sitowise.com>
2021-01-11 15:05:56 +0200

    KSR-472 Add unit tests for resetLayerTheme-function

--- src/main/client/src/utils/theme-layer/__tests__/createThemeLayer.test.js
+++ src/main/client/src/utils/theme-layer/__tests__/createThemeLayer.test.js
@@ -1,4 +1,12 @@
 import { resetLayerTheme } from '../createThemeLayer';
+import { toggleLayerLegend } from '../../../reducers/map/actions';
+
+jest.mock('../../../store');
+jest.mock('../../../reducers/map/actions');
+
+beforeEach(() => {
+    toggleLayerLegend.mockClear();
+});
 
 describe('createThemeLayer', () => {
     it('resetLayerTheme - should reset layer\'s renderer to default', () => {
@@ -17,5 +25,39 @@ describe('createThemeLayer', () => {
 
         resetLayerTheme(featureLayer, layer, [{}], jest.fn);
         expect(featureLayer).toEqual(expectedResult);
+    });
+
+    it('should close legend popup if last legend is deactivated on resetLayerTheme call', () => {
+        const featureLayer = {
+            renderer: 'themeRenderer',
+            legendEnabled: true,
+        };
+        const layer = {
+            renderer: 'defaultRenderer',
+            id: 1,
+        };
+        const otherLayer = {
+            renderer: null,
+            id: 2,
+        };
+        resetLayerTheme(featureLayer, layer, [layer, otherLayer], jest.fn, true);
+        expect(toggleLayerLegend).toBeCalledTimes(1);
+    });
+
+    it('should not close legend popup if there are active legends after resetting selected layer', () => {
+        const featureLayer = {
+            renderer: 'themeRenderer',
+            legendEnabled: true,
+        };
+        const layer = {
+            renderer: 'defaultRenderer',
+            id: 1,
+        };
+        const otherLayer = {
+            renderer: 'otherRenderer',
+            id: 2,
+        };
+        resetLayerTheme(featureLayer, layer, [layer, otherLayer], jest.fn, true);
+        expect(toggleLayerLegend).not.toBeCalled();
     });
 });
