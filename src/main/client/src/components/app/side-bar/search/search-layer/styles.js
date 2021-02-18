6a96742063ed0e0061f7e82981c7b95f3f79d111
mihuh <miika.huhtanen@sitowise.com>
2021-02-18 13:02:20 +0200

    KSR-437 Add component search result list item

--- src/main/client/src/components/app/side-bar/search/search-layer/styles.js
+++ src/main/client/src/components/app/side-bar/search/search-layer/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { InputInfo } from '../../../../ui/elements/TextInput';
+import * as styles from '../../../../ui/defaultStyles';
 
 export const LabelInfoWrapper = styled.div`
     display: flex;
@@ -12,4 +13,27 @@ export const LabelInfoWrapper = styled.div`
         right: initial;
         top: initial;
     };
+`;
+
+export const StyledSearchResultLayer = styled.div`
+    background: ${styles.colorBackgroundLight};
+    color: ${styles.colorFontDark};
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    -webkit-box-shadow: ${styles.shadowDefault};
+    -moz-box-shadow: ${styles.shadowDefault};
+    box-shadow: ${styles.shadowDefault};
+    border-left: 5px solid transparent;
+    margin: 1rem 0;
+    
+    & > div {
+        margin: 1rem;
+    }
+    
+    &:hover {
+        cursor: pointer;
+        color: ${styles.colorMainDark};
+    }
+    
 `;
