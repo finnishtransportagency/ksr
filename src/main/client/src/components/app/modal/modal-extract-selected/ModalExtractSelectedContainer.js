ed2b0ab820ec94a410459bbef8a95e71799a8e9c
Jere Pallaskorpi <jere.pallaskorpi@gmail.com>
2021-03-16 14:02:51 +0200

    KSR-586 Fix extract task for child layers

--- src/main/client/src/components/app/modal/modal-extract-selected/ModalExtractSelectedContainer.js
+++ src/main/client/src/components/app/modal/modal-extract-selected/ModalExtractSelectedContainer.js
@@ -1,15 +1,23 @@
 // @flow
 import { connect } from 'react-redux';
 import ModalExtractSelected from './ModalExtractSelected';
+import { nestedVal } from '../../../../utils/nestedValue';
 
 const mapStateToProps = (state) => {
     const selectedGeometryData = [];
     const layer = state.table.features.layers.find(l => l.id === state.table.features.activeTable);
     if (layer) layer.data.forEach(d => d._selected && selectedGeometryData.push(d.geometry));
 
+    const { layerList } = state.map.layerGroups;
     const activeTableLayer = state.map.layerGroups.layerList
         .find(l => l.id === state.table.features.activeTable && l.type === 'agfs');
-    const layerId = activeTableLayer ? activeTableLayer.id : null;
+    let layerId = activeTableLayer ? activeTableLayer.id : null;
+
+    // Use parent layers' id for child layer
+    if (layerId !== null) {
+        const parentLayer = nestedVal(layerList.find(l => l.id === layerId.replace('.s', '')), ['parentLayer']);
+        layerId = parentLayer || layerId;
+    }
 
     return {
         selectedGeometryData,
