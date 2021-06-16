2b15a94bc3ccdf1e4b33d186792d3fd0cd92325a
mihuh <miika.huhtanen@sitowise.com>
2021-06-16 08:25:48 +0300

    KSR-453 Update Tasoristeys layer names

---
+++ src/main/resources/db/migration/V20210615133310034__Update_Layer_(tasoristeys_names).sql
@@ -0,0 +1,3 @@
+UPDATE LAYER SET NAME = 'Tasoristeykset (Toijala-Valkeakoski-hanke)' WHERE NAME = 'Tasoristeykset (Toijala-Valkeakoski)';
+UPDATE LAYER SET NAME = 'Tasoristeykset (Vuokatti-Kontionmäki-hanke)' WHERE NAME = 'Tasoristeykset (Vuokatti-Kontionmäki)';
+UPDATE LAYER SET NAME = 'Tasoristeykset (Tampere-Pori-hanke)' WHERE NAME = 'Tasoristeykset (Tampere-Pori)';
