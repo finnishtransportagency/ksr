53b67c9303abbade3d90551857f49293f1c677d5
Ilmari <ilmari.tyrkko@sitowise.com>
2023-01-02 11:17:28 +0200

    KSR-672 add helpmodal to sidenav

---
+++ src/main/client/src/components/app/side-bar/helpmodal/HelpModal.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import * as styles from '../../../ui/defaultStyles';
+
+const HelpModal = styled.div`
+  padding: 4rem;
+  background: ${styles.colorBackgroundDark};
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
+  color: ${styles.colorInfo};
+`;
+
+export default HelpModal;
---
+++ src/main/client/src/components/app/side-bar/helpmodal/HelpModalContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const HelpModalContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default HelpModalContainer;
