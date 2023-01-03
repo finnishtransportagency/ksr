64e85733872edbb4538d6eb4791acd7e760c47c6
Ilmari <ilmari.tyrkko@sitowise.com>
2023-01-03 11:06:21 +0200

    KSR-672 change HelpModal layout, fix z-index issues

--- src/main/client/src/components/app/side-bar/helpmodal/HelpModalView.js
+++ src/main/client/src/components/app/side-bar/helpmodal/HelpModalView.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 import * as styles from '../../../ui/defaultStyles';
 
-const HelpModal = styled.div`
+const HelpModalView = styled.div`
   padding: 4rem;
   background: ${styles.colorBackgroundDark};
   box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1);
   color: ${styles.colorBackgroundLight};
   position: relative;
-  z-index: 9999;
+  z-index: 999999;
   
   a {
     color: ${styles.colorBackgroundLight};
   }
 `;
 
-export default HelpModal;
+export default HelpModalView;
--- src/main/client/src/components/app/side-nav/SideNavView.js
+++ src/main/client/src/components/app/side-nav/SideNavView.js
@@ -1,95 +1,85 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 import SideNav from '../../ui/blocks/SideNav/index';
 import strings from '../../../translations';
 import SideBarSubContainer from '../side-bar/SideBarSubContainer';
 import HelpModalContainer from '../side-bar/helpmodal/HelpModalContainer';
-import HelpModal from '../side-bar/helpmodal/HelpModal';
-import CloseButtonContainer from '../side-bar/helpmodal/CloseButtonContainer';
 
 type Props = {
     setActiveNav: (string) => void,
     activeNav: string,
 };
 
-const SideNavView = ({ setActiveNav, activeNav }: Props) => (
-    <React.Fragment>
-        <SideNav>
-            <SideNav.Logo onClick={() => { setActiveNav(''); }}>
-                <img src="images/VAYLAwhite.png" alt="" />
-            </SideNav.Logo>
-            <SideNav.LinkWrapper>
-                <SideBarSubContainer>
-                    <SideNav.Link
-                        title={strings.sideNav.search}
-                        active={activeNav === 'search'}
-                        onClick={() => { setActiveNav('search'); }}
-                    >
-                        <i className="fas fa-search" />
-                    </SideNav.Link>
-                    <SideNav.Link
-                        title={strings.sideNav.layerManagement}
-                        active={activeNav === 'mapLayers'}
-                        onClick={() => { setActiveNav('mapLayers'); }}
-                    >
-                        <i className="fas fa-layer-group" />
-                    </SideNav.Link>
-                    <SideNav.Link
-                        title={strings.sideNav.fileExport}
-                        active={activeNav === 'fileExport'}
-                        onClick={() => { setActiveNav('fileExport'); }}
-                    >
-                        <i className="fas fa-print" />
-                    </SideNav.Link>
-                    <SideNav.Link
-                        title={strings.sideNav.workspace}
-                        active={activeNav === 'workspace'}
-                        onClick={() => { setActiveNav('workspace'); }}
-                    >
-                        <i className="fas fa-briefcase" />
-                    </SideNav.Link>
-                    <SideNav.Link
-                        title={strings.sideNav.offline}
-                        active={activeNav === 'offline'}
-                        onClick={() => { setActiveNav('offline'); }}
-                    >
-                        <i className="fas fa-exchange-alt" />
-                    </SideNav.Link>
-                    <div style={{
-                        display: 'flex', flexDirection: 'column', flexGrow: '1', justifyContent: 'end',
-                    }}
-                    >
+const SideNavView = ({ setActiveNav, activeNav }: Props) => {
+    const [showHelpModal, setShowHelpModal] = useState(false);
+    return (
+        <React.Fragment>
+            <SideNav>
+                <SideNav.Logo onClick={() => { setActiveNav(''); }}>
+                    <img src="images/VAYLAwhite.png" alt="" />
+                </SideNav.Logo>
+                <SideNav.LinkWrapper>
+                    <SideBarSubContainer>
                         <SideNav.Link
-                            title={strings.helpModal.hoverTitle}
-                            active={activeNav === 'help'}
-                            onClick={() => {
-                                setActiveNav('help');
-                            }}
+                            title={strings.sideNav.search}
+                            active={activeNav === 'search'}
+                            onClick={() => { setActiveNav('search'); }}
                         >
-                            <i className="fas fa-question" />
+                            <i className="fas fa-search" />
                         </SideNav.Link>
-                    </div>
+                        <SideNav.Link
+                            title={strings.sideNav.layerManagement}
+                            active={activeNav === 'mapLayers'}
+                            onClick={() => { setActiveNav('mapLayers'); }}
+                        >
+                            <i className="fas fa-layer-group" />
+                        </SideNav.Link>
+                        <SideNav.Link
+                            title={strings.sideNav.fileExport}
+                            active={activeNav === 'fileExport'}
+                            onClick={() => { setActiveNav('fileExport'); }}
+                        >
+                            <i className="fas fa-print" />
+                        </SideNav.Link>
+                        <SideNav.Link
+                            title={strings.sideNav.workspace}
+                            active={activeNav === 'workspace'}
+                            onClick={() => { setActiveNav('workspace'); }}
+                        >
+                            <i className="fas fa-briefcase" />
+                        </SideNav.Link>
+                        <SideNav.Link
+                            title={strings.sideNav.offline}
+                            active={activeNav === 'offline'}
+                            onClick={() => { setActiveNav('offline'); }}
+                        >
+                            <i className="fas fa-exchange-alt" />
+                        </SideNav.Link>
+                        <div style={{
+                            display: 'flex', flexDirection: 'column', flexGrow: '1', justifyContent: 'end',
+                        }}
+                        >
+                            <SideNav.Link
+                                title={strings.helpModal.hoverTitle}
+                                active={showHelpModal}
+                                onClick={() => setShowHelpModal(!showHelpModal)}
+                            >
+                                <i className="fas fa-question" />
+                            </SideNav.Link>
+                        </div>
 
-                </SideBarSubContainer>
+                    </SideBarSubContainer>
 
-            </SideNav.LinkWrapper>
-        </SideNav>
-        {activeNav === 'help'
+                </SideNav.LinkWrapper>
+            </SideNav>
+            {showHelpModal
 
-          && (
-              <HelpModalContainer>
-                  <HelpModal>
-                      {strings.helpModal.text}
-                      {' '}
-                      <a href={`mailto:${strings.helpModal.email}`}>{strings.helpModal.email}</a>
-                      <CloseButtonContainer onClick={() => setActiveNav('')}>
-                          <i className="far fa-window-close" />
-                      </CloseButtonContainer>
-                  </HelpModal>
-              </HelpModalContainer>
-          )
-        }
-    </React.Fragment>
-);
+            && (
+                <HelpModalContainer onClickOutside={() => setShowHelpModal(false)} />
+            )
+            }
+        </React.Fragment>
+    );
+};
 
 export default SideNavView;
